e55266e081e321e05d054ac4ba9bb528e06aa18d
vhidalgo <vhohidalgo77@gmail.com>
2021-07-26 19:28:21 -0500

    Cambios EECC/CADUCO

--- workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/SQP04110Filter.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/SQP04110Filter.java
@@ -11,9 +11,12 @@ import net.miatech.beans.DBException;
  *
  * @author vhidalgo
  */
-public class SQP03952Filter extends A3959 {
-    public String VP_ACTION ="";     
-    public String VP_TICKET_NC ="";    
-    public String  VP_json_detail = "";   
+public class SQP04110Filter {
+    public String VP_CCUST = "";
+    public String VP_PROCESO = "";    
+    public String VP_FDATE1 = "";    
+    public String VP_FDATE2 = "";
+    public String VP_FEJEC = "";
+    public String VP_CDCLI= "";
     public DBException dbException = new DBException();
 }
