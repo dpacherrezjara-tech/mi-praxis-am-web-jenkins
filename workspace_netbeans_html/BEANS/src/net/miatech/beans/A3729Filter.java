991dccaaa3ed493be9893444c7fea0e02972ea1c
Carlos Tarazona <ctarazona@miatech.net>
2021-08-02 17:27:05 -0500

    Flags en Manifiesto de vuelo

--- workspace_netbeans_html/BEANS/src/net/miatech/beans/A3729Filter.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/beans/A3729Filter.java
@@ -11,16 +11,21 @@ import net.miatech.praxis.flown.A3729;
  *
  * @author ggutierrez
  */
-public class A3729Filter extends A3729{
-    
+public class A3729Filter extends A3729 {
+
     public String strTicket = "";
     public String desSTVAL = "";
     public String strFormatDate = "";
     public String strDescripcion = "";
     public String desSTVCR = "";
-
-    public String nameTxt = "";    
+    public String descFSABRE = "";
+    public String descSTASABR = "";
+    public String descFSALES = "";
     
+    public String IN_FSABRE = "";
+
+    public String nameTxt = "";
+
     public Pagination page = new Pagination();
     public long RN;
 }
