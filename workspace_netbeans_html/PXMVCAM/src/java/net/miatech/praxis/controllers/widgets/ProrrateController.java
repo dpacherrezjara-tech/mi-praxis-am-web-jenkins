6501ad062bc8a58f1ec40b768ccb201ed414fe68
Juan Diego <diegomeiggs@gmail.com>
2021-08-14 21:15:09 -0500

    cambios

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/widgets/ProrrateController.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/widgets/ProrrateController.java
@@ -36,7 +36,37 @@ import net.miatech.praxis.spring.INF020;
 @RequestMapping("/Prorate")
 public class ProrrateController extends BaseController {
     private static final Logger logError = Logger.getLogger("errorLog");
+   
+    @RequestMapping(value = "/searchDataVenta")
+    public @ResponseBody
+    String searchDataVenta(ModelMap map, HttpServletRequest request){
+       S0001A713Filter filter = new S0001A713Filter();
+       S0001A713Filter beanProrate = new S0001A713Filter();
+       List<S0001A713Filter> lstA720 = new ArrayList<>();
+       try{    
+            Functions.msjConsola("PRAXIS", this.serverSession.getServerSession().getUserView().getUserInfo().USR, getClass().getSimpleName() + " : " + Thread.currentThread().getStackTrace()[1].getMethodName());
+            filter = new Gson().fromJson(request.getParameter("beanString"), filter.getClass());
+            
+            ProrateLogic logic = new ProrateLogic();
+            logic.setSession(this.serverSession.getServerSession());
+            
+            beanProrate = logic.searchA720Data(filter);
+            lstA720 = logic.searchA720ListaCupon(filter);
+            
+            map.put("success", true);
+            map.put("beanProrate", beanProrate);          
+            map.put("lstA713",lstA720);
+            
+        } catch (SQLException e) {
+            map.put("success", false);
+            map.put("sesion", SESSION_CONTROL);
+        } catch (Exception e) {
+            map.put("success", false);
+            map.put("sesion", SESSION_CONTROL);
+        }
     
+        return new Gson().toJson(map);
+   }
    @RequestMapping(value = "/searchData")
     public @ResponseBody
     String searchData(ModelMap map, HttpServletRequest request){
