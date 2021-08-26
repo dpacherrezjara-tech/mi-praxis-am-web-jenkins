dda3f82a7e9e1c7ec896662da20c1de43f6c2ad8
vhidalgo <vhohidalgo77@gmail.com>
2021-08-26 18:29:25 -0500

    CAMBIOS Conciliacion ASR

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/sales/ConciliationASR/ConciliationASRController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/sales/ConciliationASR/ConciliationASRController.js
@@ -6,7 +6,7 @@ Ext.define('Ext.Praxis.controller.sales.ConciliationASR.ConciliationASRControlle
     beanFilter: {},
     bean_by_amount_excel: {},
     init: function(view) {
-        console.log('PERMX-ACCESS');
+        //console.log('PERMX-ACCESS');
         if(accessSelect.PERMX==='N'){
             Ext.getCmp(prototype.id+'-btnExcel').hide();
 	}else{
@@ -284,7 +284,9 @@ Ext.define('Ext.Praxis.controller.sales.ConciliationASR.ConciliationASRControlle
         }
     },
     loadPX108S02PXF053: function(filter) {
-        var storeGridDatas = Ext.create('Ext.Praxis.store.sales.GridData', {
+        
+        //var storeGridDatas = Ext.create('Ext.Praxis.store.sales.GridData', {
+        var storeGridDatas = Ext.create('Ext.Praxis.store.sales.ConciliationASR.GridDataPX031S03A1530', {
             proxy: {
                 url: prototype.url+'/loadPX108S02PXF053'
             },
@@ -301,15 +303,16 @@ Ext.define('Ext.Praxis.controller.sales.ConciliationASR.ConciliationASRControlle
         Ext.getCmp(prototype.id+'-gridDataByCurrency').setStore(storeGridDatas);
     },
     gridTransactions_itemClickHandler: function(grid, x, colIndex, e, x, rowIndex, z) {
-        if (this.gloGridTransactionsSelectedIndex === -1) {
-            this.gloGridTransactionsSelectedIndex = rowIndex;
-        } else {
-            if (rowIndex === this.gloGridTransactionsSelectedIndex) {
-                this.gloGridTransactionsSelectedIndex = -1;
-            } else {
-                this.gloGridTransactionsSelectedIndex = rowIndex;
-            }
-        }
+          this.gloGridTransactionsSelectedIndex = rowIndex;
+//        if (this.gloGridTransactionsSelectedIndex === -1) {
+//            this.gloGridTransactionsSelectedIndex = rowIndex;
+//        } else {
+//            if (rowIndex === this.gloGridTransactionsSelectedIndex) {
+//                this.gloGridTransactionsSelectedIndex = -1;
+//            } else {
+//                this.gloGridTransactionsSelectedIndex = rowIndex;
+//            }
+//        }
     },
     
     // <editor-fold defaultstate="collapsed" desc="onViewClick">
