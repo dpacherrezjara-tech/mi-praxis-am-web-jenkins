ccf86c9d7c3d76a4f3282f5cc1dc6029c4481804
Juan Diego <diegomeiggs@gmail.com>
2021-09-08 12:59:30 -0500

    TEST

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/sales/ConciliationARC/DataEntryConciliationARCController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/sales/ConciliationARC/DataEntryConciliationARCController.js
@@ -162,7 +162,8 @@ Ext.define('Ext.Praxis.controller.sales.ConciliationARC.DataEntryConciliationARC
                     fn: function() {
                         //exito
                         Ext.getCmp(prototype.id + '-dataEntry').close();
-                        Ext.getCmp(prototype.id + '-btnSearch').fireEvent('click', {});
+                        //NO ACTUALIZAR EN AUTOMATICO A PEDIDO DE BPO, SE HARA DE FORMA MANUAL AL FINALIZAR.
+                        //Ext.getCmp(prototype.id + '-btnSearch').fireEvent('click', {});
                     }
                 });
             }
