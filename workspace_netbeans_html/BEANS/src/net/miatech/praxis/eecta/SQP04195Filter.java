964864720a6b576d26a10f78b66fb3a8b6d700a0
Vladimir Hidalgo <vhohidalgo77@gmail.com>
2021-09-20 19:13:51 -0500

    ESTADO DE CUENTA

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/SQP04195Filter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.eecta;
+
+import net.miatech.beans.DBException;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class SQP04195Filter  {
+
+    public String VP_JSON_TEXT = "";
+    public DBException dbException = new DBException();
+    public String OU_A4096LOTE = "";
+
+}
