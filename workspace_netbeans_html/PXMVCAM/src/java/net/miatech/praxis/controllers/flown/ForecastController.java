1ca4c6d53f61077331150932bf3c01f3cfafda80
Carlos Tarazona <ctarazona@miatech.net>
2021-09-23 20:29:30 -0500

    Average Fare Detail en Forecast

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/flown/ForecastController.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/flown/ForecastController.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import net.miatech.beans.IMF140Filter;
 import net.miatech.beans.IMF141Filter;
+import net.miatech.beans.IMF072Filter;
 import net.miatech.praxis.controllers.BaseController;
 import net.miatech.praxis.dao.master.MasterDAO;
 import net.miatech.praxis.exceptions.SpringException;
@@ -106,6 +107,58 @@ public class ForecastController extends BaseController {
 
             //--------------------
             lst = logic.loadPX551SQP03895(filter);
+        } catch (Exception e) {
+            throw new SpringException(e);
+        }
+        return lst;
+    }
+
+    @RequestMapping(value = "searchFareDetail")
+    public @ResponseBody
+    String searchFareDetail(ModelMap map, HttpServletRequest request) {
+        System.out.println("-------------- Forecast : SearchFareDetail-------------");
+
+        map.put("success", true);
+        List<IMF072Filter> lst = this.getListFareDetail(request, false);
+        System.out.println("Total : " + lst.size());
+        map.put("total", lst.size() > 0 ? lst.get(0).page.TOTROW : 0);
+        map.put("data", lst);
+        return new Gson().toJson(map);
+    }
+
+    public List<IMF072Filter> getListFareDetail(HttpServletRequest request, Boolean bExcel) {
+
+        List<IMF072Filter> lst = new ArrayList<>(0);
+        IMF072Filter filter = new IMF072Filter();
+        Gson gson = new Gson();
+        String beanString = "";
+
+        try {
+            logic = new ForecastLogic();
+            logic.setSession(this.serverSession.getServerSession());
+
+            beanString = request.getParameter("beanString");
+            filter = gson.fromJson(beanString, IMF072Filter.class);
+
+            // Paginacion
+            filter.page.TOTROW = -1;
+            filter.page.START = 0;
+            filter.page.LIMIT = 0;
+
+            int limit = request.getParameter("limit") == null ? -1 : Integer.parseInt(request.getParameter("limit").toString());
+            int start = request.getParameter("start") == null ? 0 : Integer.parseInt(request.getParameter("start").toString());
+
+            if (!bExcel) {
+                filter.page.PAGROW = 20;
+                start = (start != 0 ? start : 0);
+                filter.page.PAGNUM = (start / filter.page.PAGROW) + 1;
+            } else {
+                filter.page.PAGROW = -1;
+                filter.page.PAGNUM = 1;
+            }
+
+            //--------------------
+            lst = logic.loadPX551SQP04159(filter);
         } catch (Exception e) {
             throw new SpringException(e);
         }
