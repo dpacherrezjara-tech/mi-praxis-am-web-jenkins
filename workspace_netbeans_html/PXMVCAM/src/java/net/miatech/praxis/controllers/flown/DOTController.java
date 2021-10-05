08f4faadce2871d21f90c230d7fa7ad7ba310243
Gianmarco <gianmgutierrez@gmail.com>
2021-10-04 21:20:55 -0500

    DOTJav

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/flown/DOTController.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/flown/DOTController.java
@@ -221,7 +221,12 @@ public class DOTController extends BaseController {
             
             map.put("success", true);
             map.put("listaArray", listaArray);
-            map.put("str", new String(bytes));
+            
+            if(listaArray.isEmpty()){
+                map.put("str", "DATA NOT FOUND ");
+            }else{
+                map.put("str", new String(bytes));
+            }
         } catch (Exception e) {
             //e.printStackTrace();
             e.printStackTrace(pw);
