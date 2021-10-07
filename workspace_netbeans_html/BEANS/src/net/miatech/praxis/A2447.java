ca3f31f03f2d13fcc1e357fccab54e643a3e9c07
Juan Diego <diegomeiggs@gmail.com>
2021-10-07 14:58:21 -0500

    TEST

--- workspace_netbeans_html/BEANS/src/net/miatech/praxis/A2447.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/A2447.java
@@ -32,5 +32,10 @@ public class A2447 {
     public String A2447FREVI = "";
     public String A2447HREVI = "";
     public String A2447INDCO = "";
-    
+    public String A2447COD = "";
+    public Double A2447COMBA = 0.00;
+    public Double A2447IVACB = 0.00;
+    public String A2447COD2 = "";
+    public Double A2447COMB2 = 0.00;
+    public Double A2447IVAC2 = 0.00;
 }
