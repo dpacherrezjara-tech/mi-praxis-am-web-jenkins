b75955a7b34d09e3f7b63876464264297b60bfdc
Carlos Tarazona <ctarazona@miatech.net>
2021-10-11 09:01:34 -0500

    Update ForecastLogic.java

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/flown/ForecastLogic.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/flown/ForecastLogic.java
@@ -80,5 +80,7 @@ public class ForecastLogic {
 
     public List<IMF140Filter> loadPX551SQP04119(IMF140Filter filter) throws SQLException, Exception {
         return ForecastDAO.loadPX551SQP04119(filter);
-    }    
+    }
+    
+    
 }
