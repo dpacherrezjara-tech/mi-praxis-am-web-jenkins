df08072b5774e2c87202543eadd0f2da0b58c567
Carlos Tarazona <ctarazona@miatech.net>
2021-10-11 09:15:03 -0500

    Update ForecastLogic.java

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/flown/ForecastLogic.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/flown/ForecastLogic.java
@@ -27,5 +27,62 @@ public class ForecastLogic {
         return ForecastDAO.loadPX551SQP04119(filter);
     }
     
+        public List<IMF140Filter> loadPX551SQP03895(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP03895(filter);
+    }
+    
+    public List<IMF072Filter> loadPX551SQP04159(IMF072Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04159(filter);
+    }
+
+    public List<IMF141Filter> loadPX551SQP03896(IMF141Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP03896(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP03897(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP03897(filter);
+    }
+    
+    public List<IMF140Filter> loadPX551SQP04160(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04160(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP03898(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP03898(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP03936(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP03936(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP03937(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP03937(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP04015(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04015(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP04016(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04016(filter);
+    }
+
+    public HashMap<String, List<IMF140Filter>> loadPX551SQP04017(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04017(filter);
+    }
+
+    public HashMap<String, List<IMF140Filter>> loadPX551SQP04096(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04096(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP04097(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04097(filter);
+    }
+
+    public List<IMF140Filter> loadPX551SQP04118(IMF140Filter filter) throws SQLException, Exception {
+        return ForecastDAO.loadPX551SQP04118(filter);
+    }
+
+    
      
 }
