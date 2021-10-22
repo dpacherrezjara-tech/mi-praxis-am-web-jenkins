0154cf39bf08956c28e4729c682edaf728316235
Vladimir Hidalgo <vhohidalgo77@gmail.com>
2021-10-22 00:47:31 -0500

    ESTADO DE CUENTA

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/A4101.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.eecta;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class A4101 {
+    public String A4101CCUST= ""; //(3) CCSID 284 NOT NULL, 
+    public String A4101FPROC= ""; //(8) CCSID 284 NOT NULL, 
+    public String A4101CDCLI= ""; //(9) CCSID 284 NOT NULL, 
+    public String A4101NLOTE= ""; //(10) CCSID 284 NOT NULL, 
+    public Integer A4101TLTTK= 0; //(9 , 0) NOT NULL, 
+    public Integer A4101TLPDF= 0 ; //(9 , 0) NOT NULL, 
+    public Integer A4101TLXML= 0 ; //(9 , 0) NOT NULL, 
+    public Integer A4101TLERR= 0 ; //(9 , 0) NOT NULL, 
+    public Integer A4101TLNFA= 0 ; //(9 , 0) NOT NULL, 
+    public Integer A4101TTLRC= 0 ; //(9 , 0) NOT NULL, 
+    public String A4101ESTAD= ""; //(1) CCSID 284 NOT NULL, 
+    public String A4101IENV= ""; //(1) CCSID 284 NOT NULL, 
+    public String A4101USRIN= ""; //(10) CCSID 284 NOT NULL, 
+    public String A4101FECIN= ""; //(8) CCSID 284 NOT NULL, 
+    public String A4101HORIN= ""; //(6) CCSID 284 NOT NULL, 
+    public String A4101USRAC= ""; //(10) CCSID 284 NOT NULL, 
+    public String A4101FECAC= ""; //(8) CCSID 284 NOT NULL, 
+    public String A4101HORAC= ""; //(6) CCSID 284 NOT NULL
+}
