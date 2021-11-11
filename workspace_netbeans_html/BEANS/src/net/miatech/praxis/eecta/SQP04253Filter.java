97cd9ca9994033dfb8a676f3906bdaa267b58dc4
VLADIMIR <vhohidalgo77@gmail.com>
2021-11-11 14:46:21 -0500

    ESTADO DE CUENTA

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/SQP04253Filter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.eecta;
+
+import net.miatech.beans.DBException;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class SQP04253Filter {
+    public String vp_ccust = "";    
+    public String vp_fproc = "";
+    public String vp_cdcli = "";
+    public DBException dbException = new DBException();
+}
