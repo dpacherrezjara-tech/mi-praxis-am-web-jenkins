82192f45c636ff973483b26ec842ee2d21be3105
Gianmarco <gianmgutierrez@gmail.com>
2022-03-01 19:40:02 -0500

    Migracion Dashboard Jv

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/screens/Dashboard01Controller.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/screens/Dashboard01Controller.java
@@ -2474,6 +2474,64 @@ public class Dashboard01Controller extends BaseController {
         }
         return lst;
     }
+    
+    
+    // ***********************************************************************
+    // ************************ DRILLDOWN ALL*********************************
+    // ***********************************************************************
+    
+     @RequestMapping(value = "searchDetail")
+    public @ResponseBody
+    String searchDetail(ModelMap map, HttpServletRequest request) {
+
+        System.out.println("-------------- Dashboard01 : searchDetail-------------");
+
+        map.put("success", true);
+        List<A1971Filter> lst = this.getListDetai(request, false);
+        System.out.println("Total : " + lst.size());
+        map.put("total", lst.size() > 0 ? lst.get(0).page.TOTROW : 0);
+        map.put("data", lst);
+        return new Gson().toJson(map);
+
+    }
+
+    public List<A1971Filter> getListDetai(HttpServletRequest request, Boolean bExcel) {
+
+        List<A1971Filter> lst = new ArrayList<>(0);
+        A1971Filter filter = new A1971Filter();
+        Gson gson = new Gson();
+        String beanString = "";
+
+        try {
+            logic = new Dashboard01Logic();
+            logic.setSession(this.serverSession.getServerSession());
+
+            beanString = request.getParameter("beanString");
+            filter = gson.fromJson(beanString, A1971Filter.class);
+            filter.page.TOTROW = -1;
+            filter.page.START = 0;
+            filter.page.LIMIT = 0;
+
+            int limit = request.getParameter("limit") == null ? -1 : Integer.parseInt(request.getParameter("limit").toString());
+            int start = request.getParameter("start") == null ? 0 : Integer.parseInt(request.getParameter("start").toString());
+
+            if (!bExcel) {
+                filter.page.PAGROW = 20;
+                start = (start != 0 ? start : 0);
+                filter.page.PAGNUM = (start / filter.page.PAGROW) + 1;
+            } else {
+                filter.page.PAGROW = -1;
+                filter.page.PAGNUM = 1;
+            }
+
+            lst = logic.loadPX246SQP00329(filter);
+
+        } catch (Exception e) {
+            throw new SpringException(e);
+        }
+        return lst;
+    }
+    
 
     // =========================================================================
     // ========================== EXPIRED ======================================
