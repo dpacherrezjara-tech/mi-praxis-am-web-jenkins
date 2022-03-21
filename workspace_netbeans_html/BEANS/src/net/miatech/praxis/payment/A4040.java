d20ff4824f8a4029c2a3fbcd636e8551ea04b554
Jesus Solano <jsolano@miatech.net>
2022-03-21 17:24:57 -0500

    A4040-A4040Filter

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/A4040.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.payment;
+
+/**
+ *
+ * @author jesus
+ */
+public class A4040 {
+public String CCUST = "";    
+public String PRDA = "";
+public String CURRENCY = "";
+public String SCOUNTRY = "";
+public String RECNBR = "";
+public String PARTEID = "";
+
+public int TOTTRAAMOU = 0;                    
+public int TOTSETAMOU = 0;     
+public int TOTPENAMOU = 0;      
+public int TOTREJAMOU = 0;   
+public int TOTTRAAMOC = 0;                    
+public int TOTSETAMOC = 0;     
+public int TOTPENAMOC = 0;      
+public int TOTREJAMOC = 0; 
+  
+
+}
---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A4040Filter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.payment.filter;
+import net.miatech.beans.Pagination;
+import net.miatech.praxis.payment.A4040;
+/**
+ *
+ * @author jesus
+ */
+public class A4040Filter extends A4040{
+   
+        public Pagination page = new Pagination();
+}
