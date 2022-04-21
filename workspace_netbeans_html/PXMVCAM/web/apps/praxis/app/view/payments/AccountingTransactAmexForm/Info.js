e96b9889a2aa26483608da1e1ec15c9b35355841
Jesus Solano <jsolano@miatech.net>
2022-04-21 12:59:01 -0500

    Create Info.js

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/AccountingTransactAmexForm/Info.js
@@ -0,0 +1,176 @@
+Ext.define('Ext.Praxis.view.payments.AccountingTransactAmexForm.Info', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.' + prototype.id + '-info',
+    layout: 'border',
+    align: 'center',
+    bodyStyle: 'background-color: #E3EAEF;',
+    defaults: {
+        bodyStyle: 'background: transparent;',
+        border: false
+    },
+    style: 'margin: 1px;',
+    items: [
+        {
+            region: 'center',
+            layout: {
+                type: 'vbox',
+                align: 'center'
+            },
+            defaults: {
+                bodyStyle: 'background: transparent;',
+                border: false,
+                width: 1200,
+                height: 700,
+                align: 'center'
+            },
+            items: [
+                {
+                    xtype: 'panel',
+                    id: prototype.id + '-panelMain',
+                    bodyStyle: 'background-color: #E3EAEF;',
+                    padding: '1',
+                    margin: '1',
+                    layout: {
+                        type: 'vbox',
+                        align: 'center'
+                    },
+                    items: [
+                        // --------------------------   GRID MAIN DATA---------------------
+                        //-----------------------------------------------------------------
+                        {
+                            xtype: 'panel',
+                            id: prototype.id + '-panelGridData',
+                            bodyStyle: 'background-color: #E3EAEF;',
+                            padding: '1',
+                            border: true,
+                            height: 560,
+                            width: 1164,
+                            layout: {
+                                type: 'vbox',
+                                align: 'center'
+                            },
+                            items: [
+                                {
+                                    xtype: 'grid',
+                                    id: prototype.id + '-gridMainAcountTransact',
+                                    height: 520,
+                                    width: 1164,
+                                    hidden: false,
+                                    columnLines: true,
+                                    columns: {
+                                        defaults: {
+                                            menuDisabled: true,
+                                            sortable: true,
+                                            align: 'center'
+                                        },
+                                        items: [
+                                            {text: 'Payment <br> Date ', dataIndex: 'PAYDATE', width: 200,
+                                                listeners: {
+                                                    click: 'onGridDetAcountTransact'
+                                                },
+                                                renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
+                                                    var data = record.data;
+                                                    metaData.style = "text-align:center;";
+                                                    value = '<b>' + value + '</b>';
+                                                    return '<a href="#payments-sales-reconcili-amex-form" style="color:#057ECB;text-decoration:underline;">' + value + '</a>';
+                                                }
+                                            },
+                                            {text: 'Transaction <br> Amount ', dataIndex: 'TGROSAMOUN', width: 200,
+                                                renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
+                                                    metaData.style = "text-align:right;background-color:#B2DAFA";
+                                                    value = Ext.util.Format.number(value, '0,000.00');
+                                                    return value;
+                                                },
+                                                summaryRenderer: function (value, summaryData, dataIndex, metaData, record) {
+                                                    var data = Ext.getCmp(prototype.id + '-gridMainAcountTransact').getStore().getData().items[0].data;
+                                                    metaData.style = 'text-align:right; margin-right:3px ';
+                                                    return '<b>' + Ext.util.Format.number(data.totTGROSAMOUN, '0,000.00') + '<b>';
+                                                }
+                                            }
+                                        ]
+                                    }
+                                },
+                                {xtype: 'tbspacer', width: 7, height: 10},
+                                {
+                                    xtype: 'panel',
+                                    id: prototype.id + '-pie',
+                                    layout: {
+                                        type: 'hbox',
+                                        pack: 'center'
+                                    },
+                                    border: true,
+                                    width: 1115,
+                                    height: 25,
+                                    bodyStyle: 'background-color: transparent; border: 1px solid #81BEF7',
+//                                    defaults: {
+//                                        border: true,
+//                                        padding: '0px 5px 0px 5px'
+//                                    },
+//                                    padding: '1px 5px 1px 5px',
+                                    items: [
+                                        {
+                                            xtype: 'panel',
+                                            width: 1164,
+                                            height: 25,
+                                            layout: {
+                                                type: 'hbox',
+                                                pack: 'center'
+                                            },
+                                            defaults: {
+                                                xtype: 'label',
+//                                                margin: '3px 0px 0px 5px'
+                                            },
+                                            items: [
+                                                {
+                                                    text: 'Page',
+                                                    width: 50
+                                                },
+                                                {
+                                                    id: prototype.id + '-lbl-currentPage',
+                                                    text: '1',
+                                                    width: 50
+                                                },
+                                                {
+                                                    text: 'Of',
+                                                    width: 50
+                                                },
+                                                {
+                                                    id: prototype.id + '-lbl-pageCount',
+                                                    text: '0',
+                                                    width: 50
+                                                },
+                                                {xtype: 'tbspacer', width: 100},
+                                                {
+                                                    text: 'Total found',
+                                                    width: 80
+                                                },
+                                                {
+                                                    id: prototype.id + '-lbl-total',
+                                                    text: '0',
+                                                    width: 50
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    region: 'south',
+                    layout: 'border',
+                    height: 0,
+                    defaults: {
+                        style: 'margin: 1px;',
+                        bodyStyle: 'background: transparent;',
+                        border: false
+                    }
+                }
+            ]
+        }
+    ]
+}
+);
+
+
