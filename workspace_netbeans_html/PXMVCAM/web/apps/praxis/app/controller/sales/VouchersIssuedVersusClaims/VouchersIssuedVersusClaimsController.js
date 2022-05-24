66ee9e857c2514bbb42a4d74cda3190b3a0127b1
VLADIMIR <vhohidalgo77@gmail.com>
2022-05-23 19:18:12 -0500

    VouchersIssuedVersusClaims(AVANCES)

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/sales/VouchersIssuedVersusClaims/VouchersIssuedVersusClaimsController.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+Ext.define('Ext.Praxis.controller.sales.VouchersIssuedVersusClaims.VouchersIssuedVersusClaimsController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.VouchersIssuedVersusClaimsController',
+    beanXLS: {},       
+    me: '',
+    setContext: function () {
+        me = this;
+    },
+    init: function (view) {
+        me = this;
+    },
+    afterRender: function () {
+        
+    },
+});
