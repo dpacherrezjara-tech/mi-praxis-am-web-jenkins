9898e8675d76efe53642a6a1e0b5ae61a97e042a
VLADIMIR <vhohidalgo77@gmail.com>
2022-05-23 19:17:23 -0500

    VouchersIssuedVersusClaims(AVANCES)

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/beans/SQP04482Filter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.beans;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class SQP04482Filter extends A4213 {
+    public int IN_TFILTER = 0;
+    public String VP_CCUST = "";
+    public String VP_TicketTV = "";
+    public String VP_TicketTK = "";
+    public String VP_TicketTVIssueDate = "";
+    public String VP_SystemDate = "";
+    public String VP_StatusFormateo = "";
+    
+    public Pagination page = new Pagination();
+}
