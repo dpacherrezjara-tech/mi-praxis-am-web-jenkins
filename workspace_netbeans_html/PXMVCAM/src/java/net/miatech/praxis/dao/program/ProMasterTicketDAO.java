b37ec2c131f140e69c747d41361cb928a01f1592
Luis Zambrano <lzambranop@gmail.com>
2022-06-19 20:58:36 -0500

    Se agrega medios de pago en Viewticket

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/program/ProMasterTicketDAO.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/program/ProMasterTicketDAO.java
@@ -24,6 +24,7 @@ import net.miatech.beans.PX040S01A720ResultSet12;
 import net.miatech.beans.PX040S01A720ResultSet13;
 import net.miatech.beans.PX040S01A720ResultSet14;
 import net.miatech.beans.PX040S01A720ResultSet15;
+import net.miatech.beans.PX040S01A720ResultSet22;
 import net.miatech.beans.PX040S02A720Filter;
 import net.miatech.beans.SQP00250Filter;
 import net.miatech.beans.SQP00697Filter;
@@ -63,6 +64,7 @@ public class ProMasterTicketDAO {
         PX040S01A720ResultSet13 objRtn13;
         PX040S01A720ResultSet14 objRtn14;
         PX040S01A720ResultSet15 objRtn15;
+        PX040S01A720ResultSet22 objRtn22;
 
         PreparedStatement pstmt01 = null, pstmt02 = null;
         ResultSet rstst01 = null, rstst02 = null;
@@ -669,6 +671,15 @@ public class ProMasterTicketDAO {
                                                                             objRtn15.fileA1747.VCPN = rs15.getDouble("VCPN");
                                                                             objRtn15.fileA1747.MDACP = rs15.getString("MDACP");
                                                                             filter.lstResultSet15.add(objRtn15);
+                                                                        }
+                                                                        if (cstmt01.getMoreResults()) {
+                                                                            rs22 = cstmt01.getResultSet();
+                                                                            while (rs22.next()) {
+                                                                                objRtn22 = new PX040S01A720ResultSet22();
+                                                                                objRtn22.fileA4121.SCARDN = rs15.getString("SCARDN");
+                                                                                objRtn22.fileA4121.SAUTHOC = rs15.getString("SAUTHOC");
+                                                                                filter.lstResultSet22.add(objRtn22);
+                                                                            }
                                                                         }
                                                                     }
                                                                 }
