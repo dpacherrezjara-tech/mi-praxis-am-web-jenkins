5f8e3e55ab7ec23bf0ce10f3656a8741deab745c
Luis Zambrano <lzambranop@gmail.com>
2022-07-06 19:49:59 -0500

    Update ProMasterTicketController.js

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/program/ProMasterTicket/ProMasterTicketController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/program/ProMasterTicket/ProMasterTicketController.js
@@ -253,7 +253,7 @@ Ext.define('Ext.Praxis.controller.program.ProMasterTicket.ProMasterTicketControl
             viewRefund.show();
 	}
 	if(data.STAT === 'FLWN'){
-            this.searchBeanTkt(win.getValue('txtFilterTicketCia').trim() + win.getValue('txtFilterTicketFormSer').trim() + data.CPN,data.SEQ, data.SEQRO);
+            this.searchBeanTkt(data.CIA + data.FOR + data.SER + data.CPN,data.SEQ, data.SEQRO);
 	}
         if(data.STAT === 'BILLED'){
             //this.searchBeanTkt(win.getValue('txtFilterTicketCia').trim() + win.getValue('txtFilterTicketFormSer').trim() + data.CPN,this.filterTKT.IN_SEQ, data.SEQ);
