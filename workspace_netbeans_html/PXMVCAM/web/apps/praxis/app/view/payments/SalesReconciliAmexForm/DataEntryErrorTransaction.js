2707598594660be95dd2c2a06479114efaa1597a
Carlos Tarazona <ctarazona@miatech.net>
2022-07-11 20:14:49 -0500

    Cambios

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesReconciliAmexForm/DataEntryErrorTransaction.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesReconciliAmexForm/DataEntryErrorTransaction.js
@@ -842,7 +842,7 @@ Ext.define('Ext.Praxis.view.payments.SalesReconciliAmexForm.DataEntryErrorTransa
                                     }
 
                                 },
-                                {xtype: 'tbspacer', width: 180},
+                                {xtype: 'tbspacer', width: 30},
                                 {
                                     xtype: 'checkboxfield',
                                     id: prototype.id + '-chkBlocked',
@@ -875,6 +875,30 @@ Ext.define('Ext.Praxis.view.payments.SalesReconciliAmexForm.DataEntryErrorTransa
                                     }
 
                                 },
+                                //150
+                                {xtype: 'tbspacer', width: 20},
+                                {
+                                    xtype: 'label',
+                                    text: 'Adjustment',
+                                    textAlign: 'center',
+                                    style: 'font-weight:bold;color:#0B333C;',
+                                    margin: '4 4 4 4',
+                                    width: 90
+                                },
+                                {xtype: 'tbspacer', width: 5},
+                                {
+                                    xtype: 'button',
+                                    width: 25,
+                                    //margin: '4 4 4 4',
+                                    iconCls: 'prx-icon-add',
+                                    //icon: 'resources/img/botones/16x16/1384382451_window_new.png',
+                                    tooltip: 'Add Adjustment',
+                                    listeners: {
+                                        click: 'addAdjustment_keyDownHandler'
+                                    }
+
+                                },
+                                {xtype: 'tbspacer', width: 10},
                             ]
                         },
                         {
@@ -989,7 +1013,7 @@ Ext.define('Ext.Praxis.view.payments.SalesReconciliAmexForm.DataEntryErrorTransa
                                     width: 25,
                                     //margin: '4 4 4 4',
                                     iconCls: 'prx-icon-clear',
-                                    tooltip: 'Add',
+                                    tooltip: 'Clean',
                                     listeners: {
                                         click: 'clear_keyDownHandler'
                                     }
@@ -1285,7 +1309,7 @@ Ext.define('Ext.Praxis.view.payments.SalesReconciliAmexForm.DataEntryErrorTransa
                                             triggerAction: 'all',
                                             valueField: 'CODE',
                                             displayField: 'NAME',
-                                            width: 130,
+                                            width: 180,
                                             labelWidth: 10,
                                             hidden: false,
                                             hiddenLabel: false
