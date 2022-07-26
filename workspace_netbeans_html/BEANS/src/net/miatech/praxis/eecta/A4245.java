eade2c07f756c853183ce65adacd0a47ae9a78a7
VLADIMIR <vhohidalgo77@gmail.com>
2022-07-25 21:39:54 -0500

    PRE-COMPRAS

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/A4245.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.eecta;
+/**
+ *
+ * @author vhidalgo
+ */
+public class A4245 {
+
+    public String A4245CCUST = "";
+    public String A4245NRRPT = "";
+    public String A4245CDCLI = "";
+    public String A4245CONTR = "";
+    public String A4245FEECC = "";
+    public String A4245REFBC = "";
+    public String A4245INDPR = "";
+    public String A4245INIPR = "";
+    public String A4245FINPR = "";
+    public String A4245FPERI = "";
+    public String A4245MDLOC = "";
+    public double A4245FARE = 0.00;
+    public double A4245IVA = 0.00;
+    public double A4245TUA = 0.00;
+    public double A4245YR = 0.00;
+    public double A4245YQ = 0.00;
+    public double A4245OTR = 0.00;
+    public double A4245TOT = 0.00;
+    public String A4245TOTLT = "";
+    public String A4245REGIS = "";
+    public String A4245FREGI = "";
+    public String A4245HREGI = "";
+    public String A4245REVIS = "";
+    public String A4245FREVI = "";
+    public String A4245HREVI = "";
+    //news
+    public String A4245STSPG = "";
+    public String A4245TIPPG = "";
+    public double A4245TOTAP = 0.00;
+    public double A4245TAJUS = 0.00;
+    public double A4245SALDP = 0.00;
+}
