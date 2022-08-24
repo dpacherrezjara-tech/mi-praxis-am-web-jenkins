902484e4731e37b0600081e4d012e662055a21a0
Diego Vicente <diegovic99@hotmail.com>
2022-08-24 17:08:14 -0500

    UATP Vistas 20220824_1

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/eecta/UATPSales/UATPSalesController.js
@@ -0,0 +1,16 @@
+Ext.define('Ext.Praxis.controller.eecta.UATPSales.UATPSalesController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.' + prototype.id + '-uatpsalescontroller',
+    // url: CONTEXTPATH + '/CargaRecibos',     
+    requires: [        
+        // 'Ext.Praxis.view.eecta.CargaRecibosForm.InfoGridComplDet'
+    ],
+    bean: {},
+    init: function (view) {
+        var me = this;
+    },    
+    afterRender: function () {            
+        // this.search_complemento();
+    },
+
+});
