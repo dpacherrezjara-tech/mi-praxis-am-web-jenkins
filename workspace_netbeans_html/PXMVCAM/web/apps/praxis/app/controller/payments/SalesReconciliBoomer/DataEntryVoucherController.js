eb86024fda69397c8e86a596744f3f3fe4598d7c
Carlos Tarazona <ctarazona@miatech.net>
2022-11-12 09:51:11 -0500

    dataentry voucher en boomer

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/SalesReconciliBoomer/DataEntryVoucherController.js
@@ -0,0 +1,81 @@
+Ext.define('Ext.Praxis.controller.payments.SalesReconciliBoomer.DataEntryVoucherController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.DataEntryVoucherController',
+    // <editor-fold defaultstate="collapsed" desc="Variables Globales">
+    meDE: '',
+    actionCode: '',
+    bean: {},
+    fecha: new Date(),
+    beanResult: {},
+    paramsExport: {},
+    lstCountry: [],
+    searchParams: {},
+    lstA1852: {},
+    dataObtain: {},
+    // </editor-fold>
+    init: function(view) {
+        prototype.id = 'SalesReconciliBoomerForm';
+        prototype.url = CONTEXTPATH + '/SalesReconciliBoomer';
+        meDE = this;
+    },
+    afterRender: function() {
+        //Llamado a base de datos para traer el recibo y monto
+    },
+    //<editor-fold defaultstate="collapsed" desc="llenarData">
+    llenarData: function(beanTemp) {
+        //Llenar el voucher - monto
+        
+        //beanTemp.IN_TDOC = '';
+        
+        meDE.paramsExport.beanString = JSON.stringify(beanTemp);
+    },
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="limpiarData">
+    limpiarData: function() {
+
+    },
+    //</editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Botones">
+    onUpdateClick: function(btn) {
+        Ext.Msg.show({
+            title: '.:PRAXIS:.',
+            msg: 'Are you sure to update ?',
+            buttons: Ext.MessageBox.YESNO,
+            scope: this,
+            icon: Ext.MessageBox.QUESTION,
+            modal: true,
+            fn: function(btn) {
+                if (btn === 'yes') {
+                    var beanTemp = {};
+                    this.llenarData(beanTemp);
+                    console.log(beanTemp.beanString);
+                    //global.getFile(prototype.url + '/getXLSXSearchDetail?beanString=' + meDE.paramsExport.beanString);
+                }
+            }
+        });
+    },
+    onCancelClick: function(btn) {
+        this.view.close();
+    },
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Utilitarios">
+    getValue: function(id) {
+        return Ext.getCmp(prototype.id + '-' + id).getValue();
+    },
+    focus: function(id) {
+        Ext.getCmp(prototype.id + '-' + id).focus();
+    },
+    setValue: function(id, txt) {
+        Ext.getCmp(prototype.id + '-' + id).setValue(txt);
+    },
+    onUpperValue: function(field, newValue, oldValue) {
+        field.setValue(newValue.toUpperCase());
+    },
+    onTextKeypress: function(obj, e, eOpts) {
+        if (e.getKey() === e.ENTER) {
+//            this.btnSearch_click();
+        }
+    }
+// </editor-fold>
+});
