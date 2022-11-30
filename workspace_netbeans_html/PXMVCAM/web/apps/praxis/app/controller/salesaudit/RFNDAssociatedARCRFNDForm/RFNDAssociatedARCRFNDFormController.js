61eaf98ce7b0b0c7482b4b5a6bc6628219777f9a
usr_mi_zperez-at-119260075650 <zperez@miatech.net>
2022-11-30 09:46:03 -0500

    zpp

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/salesaudit/RFNDAssociatedARCRFNDForm/RFNDAssociatedARCRFNDFormController.js
@@ -0,0 +1,23 @@
+
+Ext.define('Ext.Praxis.controller.salesaudit.RFNDAssociatedARCRFNDForm.RFNDAssociatedARCRFNDFormController',{
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.RFNDAssociatedARCRFNDFormController',
+
+    /**
+     * Constructor
+     */
+
+    init: function(view){
+        var me = this;
+        
+    },
+
+    /**
+     * Se ejecuta luego de haber cargado todos los componentes
+     */
+    afterRender: function(){
+        alert('Controlador cargado correctamente')
+    },
+
+});
+
