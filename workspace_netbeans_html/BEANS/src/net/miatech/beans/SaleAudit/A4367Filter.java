b401fd9114d51e76791557e9b74a6f696dd27e89
usr_mi_zperez-at-119260075650 <zperez@miatech.net>
2022-12-15 16:39:55 -0500

    zpp

--- workspace_netbeans_html/BEANS/src/net/miatech/beans/SaleAudit/A4367Filter.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/beans/SaleAudit/A4367Filter.java
@@ -5,7 +5,10 @@
  */
 package net.miatech.beans.SaleAudit;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.miatech.beans.DBException;
+import net.miatech.praxis.SaleAudit.A4367;
 
 /**
  *
@@ -16,6 +19,16 @@ public class A4367Filter {
     public String IN_DATEFROM = "";
     public String IN_DATETO = "";
     public String IN_OPTION = "";
+    
+     public String IN_PREME = "";
+     public String IN_ANIO = "";
+     public String IN_CIA = "";
+     public String IN_FORMA = "";
+     public String IN_SERIE = "";
+     public String IN_SEQ = "";
+     public String IN_CORRL = "";
+    
+     public List<A4367> lst_USOS = new ArrayList<A4367>(0);
     public net.miatech.beans.Pagination page = new net.miatech.beans.Pagination();
     public DBException dbException = new DBException();
 }
