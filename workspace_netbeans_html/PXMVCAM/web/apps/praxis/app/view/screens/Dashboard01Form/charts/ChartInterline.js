1c089fc4f693efb45673e800f3a5a8a3d26fcb6d
Jesus Solano <jsolano@miatech.net>
2023-01-10 10:55:10 -0500

    Update ChartInterline.js

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/screens/Dashboard01Form/charts/ChartInterline.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/screens/Dashboard01Form/charts/ChartInterline.js
@@ -348,7 +348,7 @@ Ext.define('Ext.Praxis.view.screens.Dashboard01Form.charts.ChartInterline', {
                                                                     var data = Ext.getCmp(prototype.id + '-gridData_INT').getStore().getData().items[0].data;
 
                                                                     metaData.style = 'text-align:right; margin-right:3px ';
-                                                                    return '<b>' + Ext.util.Format.number(data.totA050TUA, '0,000') + '<b>';
+                                                                    return '<b>' + Ext.util.Format.number(data.totA050NETO, '0,000') + '<b>';
                                                                 }
                                                             },
                                                             {text: '%', dataIndex: 'Perc1', width: 50,
