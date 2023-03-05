64a6228abf4d5b7053b4243b3e4e6812215727f8
Jesus Solano <jsolano@miatech.net>
2023-03-05 11:05:55 -0500

    clarification

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/payments/ClarificationLoadController.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/payments/ClarificationLoadController.java
@@ -84,7 +84,7 @@ public class ClarificationLoadController extends BaseController {
             if (banco.equals("EL") || banco.equals("US") || banco.equals("AX")) {
 
                 byte[] fileData2 = excelfile.getBytes();
-                msjResult = uploadCSV(fileData2, banco);
+                msjResult = uploadCSV(fileData2, banco,filename);
 
             } else if (banco.equals("STB") && input.equals("C")) {
 
@@ -503,7 +503,7 @@ public class ClarificationLoadController extends BaseController {
         return v_fecha;
     }
 
-    private String uploadCSV(byte[] bytes, String strBanco) throws Exception {
+    private String uploadCSV(byte[] bytes, String strBanco, String fileName) throws Exception {
 
         Functions.msjConsola("PRAXIS", this.serverSession.getServerSession().getUserView().getUserInfo().USR, getClass().getSimpleName() + " : " + Thread.currentThread().getStackTrace()[1].getMethodName());
 
@@ -546,8 +546,22 @@ public class ClarificationLoadController extends BaseController {
                     MONTO = fields[1].toString();
                     FECTRX = fields[11].toString();
                     MONTODISPUTA = fields[7].toString();
-                    RESPONDEEL = fields[13].toString();
-                    NROBOLAER = fields[4].toString();
+                    String FECHARespondel = fileName.substring(14,22);
+                    boolean files = (FECHARespondel != null && FECHARespondel.matches("[0-9]+"));
+                    if(files == true){
+                        RESPONDEEL = FECHARespondel;
+                    }
+                    else{
+                        System.out.println("Error en formato fecha");
+                        mensaje = "Error : Unnsopported File Name";
+                        break;
+                    }
+                    String Numb = fields[4].toString();
+                    if ((Numb.trim().length() < 13) && !(Numb.trim().equals(""))) {
+                        mensaje = "Error : Invalid Ticket";
+                        break;
+                    }
+                    NROBOLAER = Numb;
                     NROAFILICACION = fields[10].toString();
                     RESPONDEMAS = fields[12].toString();
                     MOTIVO = fields[3].toString();
@@ -557,7 +571,6 @@ public class ClarificationLoadController extends BaseController {
                     DIASREST = fields[49].toString();
                     NROREFADQUI = fields[42].toString();
                     ROC = fields[9].toString();
-                    
                     MONTO = MONTO.replace(",", "");
                     try {
                         double mt = Double.parseDouble(MONTO);
@@ -572,18 +585,11 @@ public class ClarificationLoadController extends BaseController {
                     } catch (Exception e) {
 //                        msj = " monto (I) monto No es númerico";
                     }
-
                     String RealTrama = NROCASO + "," + TARJETA + "," + MONTO + "," + FECTRX + "," + MONTODISPUTA + "," + RESPONDEEL + ","
                             + NROBOLAER + "," + NROAFILICACION + "," + RESPONDEMAS + "," + MOTIVO + "," + CODIGOMOTIVO + ","
                             + ESTATUS + "," + MONTOTRXS + "," + DIASREST + "," + NROREFADQUI + "," + ROC;
-
+                    RealTrama = RealTrama.replaceAll("\"","");
                     System.out.println(RealTrama);
-                    String Numb = fields[4].toString();
-                    if ((Numb.trim().length() < 13) && !(Numb.trim().equals(""))) {
-//                        System.out.println(Numb);
-                        mensaje = "Error : Invalid Ticket";
-                        break;
-                    }
                     listaExcelString.add(RealTrama);
                 } else {
                     System.out.println(line + ":" + fil);
