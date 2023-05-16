87ac24571b5b29aab95804ca413cf22d6022a4b7
Jesus Solano <jsolano@miatech.net>
2023-05-16 17:10:17 -0500

    cambios

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/payments/ReconciliationPaymentDAO.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/payments/ReconciliationPaymentDAO.java
@@ -3823,7 +3823,7 @@ public class ReconciliationPaymentDAO {
                 objRtn.SPNR = rs01.getString("SPNR").trim();
                 objRtn.SELLERID = rs01.getString("SELLERID").trim();
                 objRtn.SCARDN = rs01.getString("SCARDN").trim();
-                objRtn.ISREFNBR = rs01.getString("ISREFNBR").trim();
+                objRtn.ISREFNBR = rs01.getString("TICKET1").trim();
                 objRtn.DES_MERCHANT = rs01.getString("DES_MERCHANT").trim();
                 objRtn.DES_SMERCHANT = rs01.getString("DES_SMERCHANT").trim();
                 objRtn.OBSERV_BPO = rs01.getString("OBSERV_BPO").trim();
