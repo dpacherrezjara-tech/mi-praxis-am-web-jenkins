d55337cc7f6f3b050638ae7eaedcad4173fcd0e5
yamiletrg <yadiraremiciogonzales@gmail.com>
2023-06-14 17:05:58 -0500

    TaxDetailFormController - 14jun

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/sales/TaxDetailController.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/sales/TaxDetailController.java
@@ -179,8 +179,9 @@ public class TaxDetailController extends BaseController {
         bodyData.put("domain", context);
         bodyData.put("IN_USER", usr);
         bodyData.put("IN_PWD", pass);
-
-        Future<HttpResponse<JsonNode>> future = Unirest.post(urlREST.trim() + "/api/taxdetail/report001/")
+        
+//        Future<HttpResponse<JsonNode>> future = Unirest.post(urlREST.trim() + "/api/taxdetail/report001/")        
+        Future<HttpResponse<JsonNode>> future = Unirest.post(urlREST.trim() + "/api/tax-detail/taxdetail")
                 .header("content-type", "application/json")
                 .header("cache-control", "no-cache")
                 .body(new Gson().toJson(bodyData))
