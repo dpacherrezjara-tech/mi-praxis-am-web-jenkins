e1e69bb115ba1e79952336096d701558bfdc162b
nestacio <nestacio@MCR17.miatech.net>
2023-07-20 10:09:24 -0500

    Update ProMasterTicketDAO.java

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/program/ProMasterTicketDAO.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/program/ProMasterTicketDAO.java
@@ -2108,8 +2108,8 @@ public class ProMasterTicketDAO {
                 objRtn.A1531MFOP = rs01.getString("A1531MFOP");
                 objRtn.A1531CAPL = rs01.getString("A1531CAPL");
                 objRtn.A720SEQ = rs01.getString("A720SEQ");
-                objRtn.SCARDN = rs01.getString("SCARDN");
-                objRtn.SAUTHOC = rs01.getString("SAUTHOC");
+                //objRtn.SCARDN = rs01.getString("SCARDN");
+                //objRtn.SAUTHOC = rs01.getString("SAUTHOC");
                 objRtn.A720TKVOID = rs01.getString("A720TKVOID");
                 lstRtn.add(objRtn);
             }
