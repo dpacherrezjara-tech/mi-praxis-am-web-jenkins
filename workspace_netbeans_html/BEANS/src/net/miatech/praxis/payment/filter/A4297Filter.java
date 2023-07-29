f042aedacebaa67fe4808778c64af21aa91ed760
Diego Vicente <diegovic99@hotmail.com>
2023-07-29 15:43:23 -0500

    Update A4297Filter.java

--- workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A4297Filter.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A4297Filter.java
@@ -9,6 +9,7 @@ import net.miatech.praxis.payment.A4297;
 public class A4297Filter extends A4297{
     private String RN;
     private String A4451DESC1;
+    private String A4480DES;
 
     public String getRN() {
         return RN;
@@ -25,5 +26,12 @@ public class A4297Filter extends A4297{
     public void setA4451DESC1(String A4451DESC1) {
         this.A4451DESC1 = A4451DESC1;
     }
-    
+
+    public String getA4480DES() {
+        return A4480DES;
+    }
+
+    public void setA4480DES(String A4480DES) {
+        this.A4480DES = A4480DES;
+    }
 }
