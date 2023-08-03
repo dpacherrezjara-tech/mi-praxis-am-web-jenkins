8b1634e19d0f42f92a019ca184d582813f6c9e9e
Diego Vicente <diegovic99@hotmail.com>
2023-08-03 02:38:01 -0500

    Update ExportUtils.java

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/utils/ExportUtils.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/utils/ExportUtils.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import org.apache.commons.io.FileUtils;
@@ -40,7 +41,7 @@ public class ExportUtils {
             String[] nameArr = fileNameDownload.split("\\.");
             String prefix = nameArr[0];
             String suffix = "." + nameArr[1];
-            File file = File.createTempFile(prefix, suffix);
+            File file = File.createTempFile(prefix + UUID.randomUUID(), suffix);
             Sheet sheet = workbook.createSheet();
             XSSFCellStyle headerStyle = (XSSFCellStyle) workbook.createCellStyle();
             CellStyle bodyStyle = workbook.createCellStyle();
