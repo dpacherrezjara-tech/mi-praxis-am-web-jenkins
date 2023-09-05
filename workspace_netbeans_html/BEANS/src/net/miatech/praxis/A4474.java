684fa98d2f4e668e8054a306b28fb22d4c9e6041
nestacio <nestacio@MCR17.miatech.net>
2023-09-05 10:41:47 -0500

    Grid Purge

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/A4474.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis;
+
+/**
+ *
+ * @author jjulca
+ */
+public class A4474 {
+    
+    public String A4474CCUST = "";
+    public String A4474CIA = "";
+    public String A4474FORMA  = "";
+    public String A4474SERIE = "";
+    public String A4474SEQT = "";
+    public String A4474SQREG = "";
+    public String A4474FPROC = "";
+    public String A4474MODO = "";
+    public String A4474SEQRO = "";
+    public String A4474UREGI = "";
+    public String A4474FREGI = "";
+    public String A4474HREGI = "";
+
+}
