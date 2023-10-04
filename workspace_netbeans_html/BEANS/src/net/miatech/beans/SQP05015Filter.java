5d6cb1e18f9d4dc4a5ea9d112192d743d3b81adf
Alvaro Sotelo Vila <asotelo@miatech.net>
2023-10-04 11:02:02 -0500

    Revert "Revert "Merge branch 'Dev_CSR00332' into Prod_1""
    This reverts commit 0767dd6e2e0ae01258bce897237688c089fc2c6d.

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/beans/SQP05015Filter.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.beans;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class SQP05015Filter {
+
+    public String VP_FPROC1 = "";
+    public String VP_FUEN = "";
+    public String VP_PAIS = "";
+    // out
+    public Integer NHOT = 0;
+    public String COUNTRY = ""; // VARCHAR(60),
+    public String COUNTRY_CODE = ""; //VARCHAR(2),
+    public String CURR = ""; //CHAR(3),
+    
+    
+    public String PRDA1 = ""; //CHAR(8),
+    public String PRDA1_ = ""; //CHAR(8),
+    public String STATUS1 = ""; //VARCHAR(10),
+    public String ISSUDT1 = ""; //VARCHAR(60),		
+    public Integer SALE1 = 0;
+    public Integer EXCH1 = 0;
+    public Integer RFND1 = 0; //INT,
+    public Integer MEMO1 = 0; //INT,
+    public Integer VOID1 = 0; // INT,
+    public String LABEL1 = ""; //CHAR(1),
+    public String FLG1 = ""; //CHAR(3),
+
+    public String PRDA2 = ""; //CHAR(8),
+    public String PRDA2_ = ""; //CHAR(8),
+    public String STATUS2 = ""; //VARCHAR(10),
+    public String ISSUDT2 = ""; //VARCHAR(60),		
+    public Integer SALE2 = 0;
+    public Integer EXCH2 = 0;
+    public Integer RFND2 = 0; //INT,
+    public Integer MEMO2 = 0; //INT,
+    public Integer VOID2 = 0; // INT,
+    public String LABEL2 = ""; //CHAR(1),
+    public String FLG2 = ""; //CHAR(3),
+
+    public String PRDA3 = ""; //CHAR(8),
+    public String PRDA3_ = ""; //CHAR(8),
+    public String STATUS3 = ""; //VARCHAR(10),
+    public String ISSUDT3 = ""; //VARCHAR(60),		
+    public Integer SALE3 = 0;
+    public Integer EXCH3 = 0;
+    public Integer RFND3 = 0; //INT,
+    public Integer MEMO3 = 0; //INT,
+    public Integer VOID3 = 0; // INT,
+    public String LABEL3 = ""; //CHAR(1),
+    public String FLG3 = ""; //CHAR(3),
+
+    public String PRDA4 = ""; //CHAR(8),
+    public String PRDA4_ = ""; //CHAR(8),
+    public String STATUS4 = ""; //VARCHAR(10),
+    public String ISSUDT4 = ""; //VARCHAR(60),		
+    public Integer SALE4 = 0;
+    public Integer EXCH4 = 0;
+    public Integer RFND4 = 0; //INT,
+    public Integer MEMO4 = 0; //INT,
+    public Integer VOID4 = 0; // INT,
+    public String LABEL4 = ""; //CHAR(1),
+    public String FLG4 = ""; //CHAR(3),
+
+    public String PRDA5 = ""; //CHAR(8),
+    public String PRDA5_ = ""; //CHAR(8),
+    public String STATUS5 = ""; //VARCHAR(10),
+    public String ISSUDT5 = ""; //VARCHAR(60),		
+    public Integer SALE5 = 0;
+    public Integer EXCH5 = 0;
+    public Integer RFND5 = 0; //INT,
+    public Integer MEMO5 = 0; //INT,
+    public Integer VOID5 = 0; // INT,
+    public String LABEL5 = ""; //CHAR(1),
+    public String FLG5 = ""; //CHAR(3),
+
+    public String PRDA6 = ""; //CHAR(8),
+    public String PRDA6_ = ""; //CHAR(8),
+    public String STATUS6 = ""; //VARCHAR(10),
+    public String ISSUDT6 = ""; //VARCHAR(60),		
+    public Integer SALE6 = 0;
+    public Integer EXCH6 = 0;
+    public Integer RFND6 = 0; //INT,
+    public Integer MEMO6 = 0; //INT,
+    public Integer VOID6 = 0; // INT,
+    public String LABEL6 = ""; //CHAR(1),
+    public String FLG6 = ""; //CHAR(3),
+
+    public String PRDA7 = ""; //CHAR(8),
+    public String PRDA7_ = ""; //CHAR(8),
+    public String STATUS7 = ""; //VARCHAR(10),
+    public String ISSUDT7 = ""; //VARCHAR(60),		
+    public Integer SALE7 = 0;
+    public Integer EXCH7 = 0;
+    public Integer RFND7 = 0; //INT,
+    public Integer MEMO7 = 0; //INT,
+    public Integer VOID7 = 0; // INT,
+    public String LABEL7 = ""; //CHAR(1),
+    public String FLG7 = ""; //CHAR(3),
+}
