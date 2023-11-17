46d105d79b55a969258af82c96cbb55719513c0b
VLADIMIR <vhohidalgo77@gmail.com>
2023-11-16 22:02:45 -0500

    CAMBIOS EECC PRECOMPRAS

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/A4243.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.eecta;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class A4243 {
+    public String A4243CCUST = "";
+    public String A4243CIA = "";
+    public String A4243FORMA = "";
+    public String A4243SERIE = ""; //HAR(6) CCSID 284 NOT NULL, 
+    public String A4243SEQ = ""; //HAR(2) CCSID 284 NOT NULL, 
+    public String A4243GRUPO = ""; //HAR(9) CCSID 284 NOT NULL, 
+    public String A4243TRNCU = ""; //HAR(4) CCSID 284 NOT NULL, 
+    public Integer A4243CORRL = 0;
+    public String A4243FCONT = ""; //HAR(8) CCSID 284 NOT NULL, 
+    public String A4243ARCH = ""; //HAR(10) CCSID 284 NOT NULL, 
+    public String A4243CAMPO = ""; //HAR(10) CCSID 284 NOT NULL, 
+    public String A4243CODER = ""; //HAR(6) CCSID 284 NOT NULL, 
+    public String A4243DATA = ""; //HAR(80) CCSID 284 NOT NULL, 
+    public String A4243STSER = ""; //HAR(1) CCSID 284 NOT NULL, 
+    public String A4243TIPCO = ""; //HAR(1) CCSID 284 NOT NULL, 
+    public String A4243RUT = ""; //HAR(10) CCSID 284 NOT NULL, 
+    public String A4243KEY = ""; //HAR(50) CCSID 284 NOT NULL, 
+    public String A4243USRFZ = ""; //HAR(10) CCSID 284 NOT NULL, 
+    public String A4243FECFZ = ""; //HAR(8) CCSID 284 NOT NULL, 
+    public String A4243HORFZ = ""; //HAR(6) CCSID 284 NOT NULL, 
+    public String A4243USRIN = ""; //HAR(10) CCSID 284 NOT NULL, 
+    public String A4243FECIN = ""; //HAR(8) CCSID 284 NOT NULL, 
+    public String A4243HORIN = ""; //HAR(6) CCSID 284 NOT NULL, 
+    public String A4243USRAC = ""; //HAR(10) CCSID 284 NOT NULL, 
+    public String A4243FECAC = ""; //HAR(8) CCSID 284 NOT NULL, 
+    public String A4243HORAC = ""; //HAR(6) CCSID 284 NOT NULL
+}
---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/eecta/ControlUATPPreForm/ControlUATPPreProcesarForm.js
@@ -0,0 +1,268 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Ext.define('Ext.Praxis.view.eecta.ControlUATPPreForm.ControlUATPPreProcesarForm', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.' + prototype.id02 + '-dataEntry',
+    controller: prototype.id02 + '-controlUATPPreProcesarController',
+    requires: [
+        'Ext.Praxis.controller.eecta.ControlUATPPre.ControlUATPPreProcesarController'
+    ],
+    title: 'Procesar',
+    header: true,
+    width: 500,
+    height: 180,
+    border: false,
+    resizable: false,
+    layout: {
+        type: 'border',
+        align: 'center'
+    },
+    modal: true,
+    items: [
+        {
+            region: 'center',
+            xtype: 'form',
+            id: prototype.id02 + '-DataEntry-center',
+            border: false,
+            layout: {
+                type: 'vbox'
+            },
+            items: [
+                {
+                    xtype: 'panel',
+                    id: prototype.id02 + '-form01',
+                    layout: 'hbox',
+                    width: '100%',
+                    margin: '5 0 0 0',
+                    items: [
+                        {
+                            xtype: 'panel',
+                            layout: 'hbox',
+                            border: false,
+                            margin: '1 1 1 1',
+                            width: 150,
+                            items: [
+                                {
+                                    xtype: 'radiofield',
+                                    id: prototype.id02 + '-op01',
+                                    name: prototype.id02 + '-op',
+                                    boxLabel: 'REPORTE VENTAS',
+                                    margin: '2 2 2 10',
+                                    checked: true
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'panel',
+                            layout: 'hbox',
+                            border: false,
+                            margin: '1 1 1 1',
+                            items: [
+                                {
+                                    xtype: 'datefield',
+                                    id: prototype.id02 + '-FECHA1',
+                                    fieldLabel: 'Seleccionar fecha', labelAlign: 'right', labelStyle: 'font-weight: bold;', labelWidth: 120,
+                                    width: 230, fieldStyle: 'font-weight: bold;font-size:13px;text-align:center',
+                                    format: 'Ymd',
+                                    invalidText: 'Ingrese fecha valida en formato Ymd',
+                                    minValue: new Date(1990, 00, 01),
+                                    maxValue: new Date(),
+                                    value: new Date(),
+                                    maskRe: /[0-9/]/,
+                                    editable: true,
+                                    enableKeyEvents: true,
+                                    enforceMaxLength: true,
+                                    maxLength: 12,
+                                    listeners: {
+                                        keypress: function (obj, e) {
+                                            if (e.getKey() === e.ENTER) {
+                                                Ext.getCmp(prototype.id02 + '-FECHA2').focus();
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    xtype: 'datefield',
+                                    id: prototype.id02 + '-FECHA2',
+                                    fieldLabel: 'Hasta', labelAlign: 'right', labelStyle: 'font-weight: bold;', labelWidth: 50,
+                                    width: 160, fieldStyle: 'font-weight: bold;font-size:13px;text-align:center',
+                                    format: 'Ymd', disabled: true,
+                                    invalidText: 'Ingrese fecha valida en formato Ymd',
+                                    minValue: new Date(1990, 00, 01),
+                                    maxValue: new Date(),
+                                    value: new Date(),
+                                    hidden: true,
+                                    maskRe: /[0-9/]/,
+                                    editable: true,
+                                    enableKeyEvents: true,
+                                    enforceMaxLength: true,
+                                    maxLength: 12,
+                                    listeners: {
+                                        keypress: function (obj, e) {
+                                            if (e.getKey() === e.ENTER) {
+                                                //Ext.getCmp(prototype.id + '-btn-save').focus();
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        }                        
+                    ]
+                },
+                {
+                    xtype: 'panel',
+                    id: prototype.id02 + '-form02',
+                    hidden: true,
+                    layout: 'hbox',
+                    width: '100%',
+                    border: false,
+                    margin: '5 5 5 5',
+                    items: [
+                        {
+                            xtype: 'panel',
+                            layout: 'hbox',
+                            border: false,
+                            margin: '1 1 1 1',
+                            width: 150,
+                            items: [{
+                                    xtype: 'radiofield',
+                                    id: prototype.id02 + '-op02',
+                                    name: prototype.id02 + '-op',
+                                    boxLabel: 'REPORTE DE VENTA',
+                                    margin: '2 2 2 10'
+                                }]
+
+                        },
+                        {
+                            xtype: 'panel',
+                            layout: 'hbox',
+                            border: false,
+                            margin: '1 1 1 1',
+                            items: [
+                                {
+                                    xtype: 'datefield',
+                                    id: prototype.id02 + '-FECHEJE01',
+                                    fieldLabel: 'Emisión', labelAlign: 'right', labelStyle: 'font-weight: bold;', labelWidth: 70,
+                                    width: 180, fieldStyle: 'font-weight: bold;font-size:13px;text-align:center',
+                                    format: 'Ymd',
+                                    invalidText: 'Ingrese fecha valida en formato Ymd',
+                                    minValue: new Date(1990, 00, 01),
+                                    maxValue: new Date(),
+                                    value: new Date(),
+                                    maskRe: /[0-9/]/,
+                                    editable: true,
+                                    enableKeyEvents: true,
+                                    enforceMaxLength: true,
+                                    maxLength: 12,
+                                    listeners: {
+                                        keypress: function (obj, e) {
+                                            if (e.getKey() === e.ENTER) {
+                                                Ext.getCmp(prototype.id + '-FECHA2').focus();
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    xtype: 'panel',
+                    id: prototype.id02 + '-form03',
+                    layout: 'hbox',
+                    width: '100%',
+                    margin: '5 0 0 0',
+                    items: [
+                        {
+                            xtype: 'panel',
+                            layout: 'hbox',
+                            border: false,
+                            margin: '1 1 1 1',
+                            width: 150,
+                            items: [
+                                {
+                                    xtype: 'radiofield',
+                                    id: prototype.id02 + '-op03',
+                                    name: prototype.id02 + '-op',
+                                    boxLabel: 'ESTADO DE CUENTA',
+                                    margin: '2 2 2 10'
+                                }]
+
+                        },
+                        {
+                            xtype: 'panel',
+                            layout: 'hbox',
+                            border: false,
+                            margin: '1 1 1 1',
+                            items: [
+                                {
+                                    xtype: 'datefield',
+                                    id: prototype.id02 + '-FECHEJE02',
+                                    fieldLabel: 'Fecha emisión', labelAlign: 'right', labelStyle: 'font-weight: bold;', labelWidth: 120,
+                                    width: 230, fieldStyle: 'font-weight: bold;font-size:13px;text-align:center',
+                                    format: 'Ymd',
+                                    invalidText: 'Ingrese fecha valida en formato Ymd',
+                                    minValue: new Date(1990, 00, 01),
+                                    maxValue: new Date(),
+                                    value: new Date(),
+                                    maskRe: /[0-9/]/,
+                                    editable: true,
+                                    enableKeyEvents: true,
+                                    enforceMaxLength: true,
+                                    maxLength: 12,
+                                    listeners: {
+                                        keypress: function (obj, e) {
+                                            if (e.getKey() === e.ENTER) {
+                                                Ext.getCmp(prototype.id + '-FECHA2').focus();
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    xtype: 'label',
+                    padding: '0 2 2 5',
+                    html: '<font color="green"><h3 id="ControlUATPPreProcesarForm_Msg"></h3></font>'
+                }
+            ]
+        }
+    ],
+    dockedItems: [
+        {
+            xtype: 'toolbar',
+            dock: 'bottom',
+            border: true,
+            ui: 'footer',
+            margin: '1 1 1 1', // left/right/top/botton
+            defaults: {
+                scale: 'medium'
+            },
+            style: 'aling:center padding: 5px;',
+            items: [
+                {
+                    text: 'Ejecutar',
+                    id: prototype.id02 + '-btn-save',
+                    iconCls: 'prx-icon-processing',
+                    listeners: {
+                        click: 'onSaveClick'
+                    }
+                },
+                {
+                    text: 'Close',
+                    id: prototype.id02 + '-btn-cancel',
+                    iconCls: 'prx-icon-cancel',
+                    listeners: {
+                        click: 'onCancelClick'
+                    }
+                }
+            ]
+        }
+    ]
+});
