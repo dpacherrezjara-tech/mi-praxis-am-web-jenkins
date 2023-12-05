9de2c33c86086b8ea7d825db89ac1ed0ca8022c3
Alvaro Sotelo Vila <asotelo@miatech.net>
2023-12-05 10:02:21 -0500

    Revert "Revert "Merge branch 'Dev_MediosDePago' into Test_1""
    This reverts commit c42d1480d0a0483df251b80943aea15ea4287aa1.

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesReconciliationControlForm/DataEntrys/BPOProductionDataEntry.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesReconciliationControlForm/DataEntrys/BPOProductionDataEntry.js
@@ -48,7 +48,7 @@ Ext.define('Ext.Praxis.view.payments.SalesReconciliationControlForm.DataEntrys.B
                             store: Ext.create('Ext.data.SimpleStore', {
                                 fields: ['code', 'name'],
                                 data: [
-                                    ['FEUP', 'Date Worked']
+                                    ['FEUP', 'Worked Date']
                                 ]
                             }),
                             labelWidth: 80,
