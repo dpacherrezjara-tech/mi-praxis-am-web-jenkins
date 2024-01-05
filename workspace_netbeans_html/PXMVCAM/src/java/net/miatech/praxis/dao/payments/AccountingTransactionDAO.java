dff1e80f67a1d4814a84812b5b754169738c94db
Diego Vicente <diegovic99@hotmail.com>
2024-01-05 13:01:04 -0500

    cambios en conexion 2

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/payments/AccountingTransactionDAO.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/dao/payments/AccountingTransactionDAO.java
@@ -1,9 +1,7 @@
 package net.miatech.praxis.dao.payments;
 
-import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
-import net.miatech.praxis.classes.CurrentSession;
 import net.miatech.praxis.logic.payments.AccountingTransactionLogic;
 import net.miatech.praxis.payment.entities.A4451MP;
 import net.miatech.praxis.payment.filter.A4183Filter;
@@ -17,14 +15,12 @@ import net.miatech.praxis.payment.filter.SQP05037Filter;
 import net.miatech.praxis.payment.filter.SQP05041Filter;
 import net.miatech.praxis.payment.filter.SQP05042Filter;
 import net.miatech.praxis.payment.filter.SQP05043Filter;
+import net.miatech.praxis.utils.JdbcUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.stereotype.Service;
 
 /**
@@ -35,61 +31,44 @@ import org.springframework.stereotype.Service;
 @Scope("session")
 public class AccountingTransactionDAO implements AccountingTransactionLogic{
     
-    //<editor-fold defaultstate="collapsed" desc="inject">
     @Autowired
-    private CurrentSession session;
+    private JdbcUtils jdbcUtils;
 
-    private JdbcTemplate getConnection() throws Exception {
-        Connection cnx = session.getServerSession().getCNXIBMDB2().getIBMDB2Connection();
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(cnx, false));
-        return jdbcTemplate;
-    } 
-//</editor-fold>
+    private static final String LIBRARY = "PRAXISMP";
 
     @Override
     public SQP05036Filter getSQP05036Filter(SQP05036Filter filter) throws Exception {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(this.getConnection())
-                .withSchemaName("PRAXISMP")
-                .withProcedureName("SQP05036")
-                .returningResultSet("result", new BeanPropertyRowMapper(A4331AT1Filter.class));
         SqlParameterSource params = new BeanPropertySqlParameterSource(filter);
-        Map<String, Object> obj = jdbcCall.execute(params);
+        Map<String, Object> obj = jdbcUtils.executeSQP(LIBRARY, "SQP05036", params,
+                new BeanPropertyRowMapper<>(A4331AT1Filter.class));
         filter.setResponse((List<A4331AT1Filter>) obj.get("result"));
         return filter;
     }
 
     @Override
     public SQP05004Filter getSQP05004Filter(SQP05004Filter filter) throws Exception {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(this.getConnection())
-                .withSchemaName("PRAXISMP")
-                .withProcedureName("SQP05004")
-                .returningResultSet("result", new BeanPropertyRowMapper<>(A4451MP.class));
         SqlParameterSource params = new BeanPropertySqlParameterSource(filter);
-        filter.setLst((List<A4451MP>) jdbcCall.execute(params).get("result"));
+        Map<String, Object> obj = jdbcUtils.executeSQP(LIBRARY, "SQP05004", params,
+                new BeanPropertyRowMapper<>(A4451MP.class));
+        filter.setLst((List<A4451MP>) obj.get("result"));
         return filter;
     }
 
     @Override
     public SQP05037Filter getSQP05037Filter(SQP05037Filter filter) throws Exception {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(this.getConnection())
-                .withSchemaName("PRAXISMP")
-                .withProcedureName("SQP05037")
-                .returningResultSet("result", new BeanPropertyRowMapper(A4331AT2Filter.class));
         SqlParameterSource params = new BeanPropertySqlParameterSource(filter);
-        Map<String, Object> obj = jdbcCall.execute(params);
+        Map<String, Object> obj = jdbcUtils.executeSQP(LIBRARY, "SQP05037", params,
+                new BeanPropertyRowMapper<>(A4331AT2Filter.class));
         filter.setResponse((List<A4331AT2Filter>) obj.get("result"));
         return filter;
     }
 
     @Override
     public SQP05041Filter getSQP05041Filter(SQP05041Filter filter) throws Exception {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(this.getConnection())
-                        .withSchemaName("PRAXISMP")
-                        .withProcedureName("SQP05041")
-                        .returningResultSet("result", new BeanPropertyRowMapper(A4331Filter.class));
         filter.setPage();
         SqlParameterSource params = new BeanPropertySqlParameterSource(filter);
-        Map<String, Object> obj = jdbcCall.execute(params);
+        Map<String, Object> obj = jdbcUtils.executeSQP(LIBRARY, "SQP05041", params,
+                new BeanPropertyRowMapper<>(A4331Filter.class));
         filter.setResponse((List<A4331Filter>) obj.get("result"));
         filter.setPageOut(obj);
         return filter;
@@ -97,24 +76,18 @@ public class AccountingTransactionDAO implements AccountingTransactionLogic{
 
     @Override
     public SQP05042Filter getSQP05042Filter(SQP05042Filter filter) throws Exception {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(this.getConnection())
-                        .withSchemaName("PRAXISMP")
-                        .withProcedureName("SQP05042")
-                        .returningResultSet("result", new BeanPropertyRowMapper(A4183Filter.class));
         SqlParameterSource params = new BeanPropertySqlParameterSource(filter);
-        Map<String, Object> obj = jdbcCall.execute(params);
+        Map<String, Object> obj = jdbcUtils.executeSQP(LIBRARY, "SQP05042", params,
+                new BeanPropertyRowMapper<>(A4183Filter.class));
         filter.setResponse((List<A4183Filter>) obj.get("result"));
         return filter;
     }
 
     @Override
     public SQP05043Filter getSQP05043Filter(SQP05043Filter filter) throws Exception {
-        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(this.getConnection())
-                        .withSchemaName("PRAXISMP")
-                        .withProcedureName("SQP05043")
-                        .returningResultSet("result", new BeanPropertyRowMapper(A4335Filter.class));
         SqlParameterSource params = new BeanPropertySqlParameterSource(filter);
-        Map<String, Object> obj = jdbcCall.execute(params);
+        Map<String, Object> obj = jdbcUtils.executeSQP(LIBRARY, "SQP05043", params,
+                new BeanPropertyRowMapper<>(A4335Filter.class));
         filter.setResponse((List<A4335Filter>) obj.get("result"));
         return filter;
     }
