3cce1181e3f257dd9663dac54dd992e15de7ff73
VLADIMIR <vhohidalgo77@gmail.com>
2024-01-24 15:52:35 -0500

    PRECOMPRA

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/SQP05212Filter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.eecta;
+
+import net.miatech.beans.DBException;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class SQP05212Filter {
+
+    public String VP_CCUST = ""; // CHAR(3),
+    public Integer VP_IDANT = 0; //INT,
+    public String VP_FDESDE = ""; //VARCHAR(8), --no_usado
+    public String VP_FHASTA = ""; //VARCHAR(8), --no_usado
+    public String VP_CDCLI = ""; //VARCHAR(9),  --no_usado    
+//  OUT OU_SQLCODE VARCHAR(7),
+//  OUT OU_MESSAGE VARCHAR(60) 
+//  Out Message SQL
+    public DBException dbException = new DBException();
+}
---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/eecta/SQP05242Filter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.miatech.praxis.eecta;
+
+/**
+ *
+ * @author vhidalgo
+ */
+public class SQP05242Filter extends A4549 {
+
+    public String VP_CCUST = "";
+    public Integer VP_IDANT = 0;
+    
+    public String path = "";
+    public String fileName = "";    
+}
