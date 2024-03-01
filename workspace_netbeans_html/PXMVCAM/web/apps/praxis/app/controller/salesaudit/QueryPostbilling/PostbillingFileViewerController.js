e8ebb47b70de67d440737103cc6b41429de2689a
usr_mi_zperez-at-119260075650 <zperez@miatech.net>
2024-03-01 16:51:51 -0500

    zpp

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/salesaudit/QueryPostbilling/PostbillingFileViewerController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/salesaudit/QueryPostbilling/PostbillingFileViewerController.js
@@ -90,19 +90,19 @@ Ext.define('Ext.Praxis.controller.salesaudit.QueryPostbilling.PostbillingFileVie
             success: function (response, options) {
                 //   Ext.getCmp('DisputeFileViewer').unmask();
                 var res = Ext.JSON.decode(response.responseText);
-                me.beanDataima = res;
+                me.beanDataima = res.data;
                 //console.log(res.map.files.myArrayList);
                 //var data = Ext.JSON.decode(res.data);
                 //console.log(data);
 
                 var dataRoot = {text: me.beanTMP.IN_DOCUMENT, filename: '', expanded: true, flag: false, children: []};
 
-                Ext.Object.each(res.map.files.myArrayList, function (index, value) {
-                    var vd = value.map.url.split('/');
+                Ext.Object.each(res.data, function (index, value) {
+                    var vd = value.url.split('/');
                     dataRoot.children.push({
                         leaf: true,
-                        text: vd[5], //value.map.url,
-                        filename: value.map.url,
+                        text: vd[0], //value.map.url,
+                        filename: value.url,
                         flag: true
                     });
                 });
