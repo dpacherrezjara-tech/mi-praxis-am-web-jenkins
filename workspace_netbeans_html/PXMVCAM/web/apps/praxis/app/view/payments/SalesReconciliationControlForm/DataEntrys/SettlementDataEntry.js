e9ce7514ee93a7945b1a5d8faf6c1f59076f9118
Diego Vicente <diegovic99@hotmail.com>
2024-04-11 18:24:27 -0500

    Settlement DataEntry 2

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesReconciliationControlForm/DataEntrys/SettlementDataEntry.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesReconciliationControlForm/DataEntrys/SettlementDataEntry.js
@@ -4,8 +4,9 @@ Ext.define('Ext.Praxis.view.payments.SalesReconciliationControlForm.DataEntrys.S
     extend: 'Ext.window.Window',
     alias: 'widget.SettlementDataEntry',
     requires: [
+        'Ext.Praxis.controller.payments.SalesReconciliationControl.SettlementDataEntryController'
     ],
-    //controller: 'TransacErrorBPODataEntryController',
+    controller: 'SettlementDataEntryController',
     title: 'Settlement - Form',
     header: true,
     width: 1075,
@@ -69,28 +70,270 @@ Ext.define('Ext.Praxis.view.payments.SalesReconciliationControlForm.DataEntrys.S
                                     width: 230
                                 },
                                 {
-                                    fieldLabel: 'P. Merchant ID',
-                                    name: 'pmerchid',
-                                    labelWidth: 120,
-                                    width: 230
+                                    fieldLabel: 'Payment Date',
+                                    name: 'paydate',
+                                    labelWidth: 100,
+                                    width: 180
                                 },
                                 {
-                                    fieldLabel: 'S. Merchant ID',
+                                    fieldLabel: 'Processor',
+                                    name: 'desc_PROC',
+                                    labelWidth: 80,
+                                    width: 240
+                                },
+                                {
+                                    fieldLabel: 'Country',
+                                    name: 'scountry',
+                                    labelWidth: 60,
+                                    width: 110
+                                },
+                                {
+                                    fieldLabel: 'Trans. Type',
+                                    name: 'transtype',
+                                    labelWidth: 85,
+                                    width: 155
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: '<span style="font-weight: bold; text-decoration-line: underline;font-size:13px;">Merchant</span>',
+                    items: [
+                        {
+                            items: [
+                                {
+                                    fieldLabel: 'Sale Merchant',
                                     name: 'smerchid',
                                     labelWidth: 120,
                                     width: 230
                                 },
                                 {
-                                    fieldLabel: 'Inst. Plan',
-                                    name: 'nbrinsta',
-                                    labelWidth: 70,
-                                    width: 120
+                                    name: 'desc_PMERCHID',
+                                    width: 230
                                 },
                                 {
-                                    fieldLabel: 'Inst. Nbr',
+                                    fieldLabel: 'Payment Merchant',
+                                    name: 'pmerchid',
+                                    labelWidth: 120,
+                                    width: 230
+                                },
+                                {
+                                    name: 'desc_SMERCHID',
+                                    width: 230
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: '<span style="font-weight: bold; text-decoration-line: underline;font-size:13px;">Sale Information</span>',
+                    items: [
+                        {
+                            items: [
+                                {
+                                    fieldLabel: 'Sale Date',
+                                    name: 'sdate',
+                                    labelWidth: 120,
+                                    width: 230
+                                },
+                                {
+                                    fieldLabel: 'Ticket',
+                                    name: 'ticket',
+                                    labelWidth: 60,
+                                    width: 180
+                                },
+                                {
+                                    fieldLabel: 'PNR',
+                                    name: 'spnr',
+                                    labelWidth: 40,
+                                    width: 130
+                                },
+                                {
+                                    fieldLabel: 'Qty. Tkts',
+                                    name: 'qtytkt',
+                                    labelWidth: 80,
+                                    width: 150
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: '<span style="font-weight: bold; text-decoration-line: underline;font-size:13px;">Credit Card Information</span>',
+                    items: [
+                        {
+                            items: [
+                                {
+                                    fieldLabel: 'Card Type',
+                                    name: 'payplatype',
+                                    labelWidth: 120,
+                                    width: 230,
+                                    listeners: {
+                                        change: function (field, newValue) {
+                                            const opts = {
+                                                'D': 'Debit',
+                                                'C': 'Credit'
+                                            };
+                                            field.setRawValue(opts[newValue] || 'No Info');
+                                        }
+                                    }
+                                },
+                                {
+                                    fieldLabel: 'Inst. Plan',
+                                    name: 'nbrinsta',
+                                    labelWidth: 80,
+                                    width: 150
+                                },
+                                {
+                                    fieldLabel: 'Inst. Number',
                                     name: 'instanbr',
+                                    labelWidth: 90,
+                                    width: 160
+                                },
+                                {
+                                    fieldLabel: 'Card Number',
+                                    name: 'scardn',
+                                    labelWidth: 90,
+                                    width: 220
+                                },
+                                {
+                                    fieldLabel: 'Auth Code',
+                                    name: 'sauthoc',
                                     labelWidth: 70,
-                                    width: 120
+                                    width: 150
+                                }
+
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: '<span style="font-weight: bold; text-decoration-line: underline;font-size:13px;">Amounts/Commissions</span>',
+                    items: [
+                        {
+                            items: [
+                                {
+                                    fieldLabel: 'Trans. Amount',
+                                    name: 'tgrosamoun',
+                                    labelWidth: 120,
+                                    width: 230,
+                                    listeners: {
+                                        change: function (field, newValue) {
+                                            field.setRawValue(Ext.util.Format.number(newValue, '0,000.00'));
+                                        }
+                                    }
+
+                                },
+                                {
+                                    fieldLabel: 'Currency',
+                                    name: 'scurrency',
+                                    labelWidth: 70,
+                                    width: 140
+                                },
+
+                                {
+                                    fieldLabel: 'Payment Amount',
+                                    name: 'tgrosampay',
+                                    labelWidth: 120,
+                                    width: 230,
+                                    listeners: {
+                                        change: function (field, newValue) {
+                                            field.setRawValue(Ext.util.Format.number(newValue, '0,000.00'));
+                                        }
+                                    }
+
+                                },
+                                {
+                                    fieldLabel: 'P. Currency',
+                                    name: 'pcurrency',
+                                    labelWidth: 80,
+                                    width: 150
+                                },
+                                {
+                                    fieldLabel: 'NET Amount',
+                                    name: 'netopay',
+                                    labelWidth: 90,
+                                    fieldStyle: 'background-color:#8AE884;font-weight:bold;text-align:center;',
+                                    width: 200,
+                                    listeners: {
+                                        change: function (field, newValue) {
+                                            field.setRawValue(Ext.util.Format.number(newValue, '0,000.00'));
+                                        }
+                                    }
+
+                                }
+
+                            ]
+                        },
+                        {
+                            items: [
+                                {
+                                    fieldLabel: 'MSI Rate',
+                                    name: 'sfeerate',
+                                    labelWidth: 120,
+                                    width: 230,
+                                    listeners: {
+                                        change: function (field, newValue) {
+                                            field.setRawValue(newValue + '%');
+                                        }
+                                    }
+                                },
+                                {
+                                    fieldLabel: 'MSI Fee',
+                                    name: 'servicfeep',
+                                    labelWidth: 120,
+                                    width: 230
+                                },
+                                {
+                                    fieldLabel: 'MSI Comm',
+                                    name: 'acceamouc',
+                                    labelWidth: 120,
+                                    width: 230
+                                },
+                                {
+                                    fieldLabel: 'MSI VAT',
+                                    name: 'overcom12p',
+                                    labelWidth: 130,
+                                    width: 230
+                                }
+                            ]
+                        },
+                        {
+                            items: [
+                                {
+                                    fieldLabel: 'Comm. Rate',
+                                    name: 'discrate',
+                                    labelWidth: 120,
+                                    width: 230,
+                                    listeners: {
+                                        change: function (field, newValue) {
+                                            field.setRawValue(newValue + '%');
+                                        }
+                                    }
+                                },
+                                {
+                                    fieldLabel: 'Comm. Amount',
+                                    name: 'sfeeamou',
+                                    labelWidth: 120,
+                                    width: 230
+                                },
+                                {
+                                    fieldLabel: 'VAT Rate',
+                                    name: 'discratei',
+                                    labelWidth: 120,
+                                    width: 230,
+                                    listeners: {
+                                        change: function (field, newValue) {
+                                            field.setRawValue(newValue + '%');
+                                        }
+                                    }
+                                },
+                                {
+                                    fieldLabel: 'Comm. VAT Amount',
+                                    name: 'ivacom12',
+                                    labelWidth: 130,
+                                    width: 230
                                 }
                             ]
                         }
