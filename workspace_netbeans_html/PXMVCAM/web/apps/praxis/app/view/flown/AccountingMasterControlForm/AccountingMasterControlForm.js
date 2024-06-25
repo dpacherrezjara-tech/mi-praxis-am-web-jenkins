99b9c3bb75d3b33f2803d26ae48d3cee5e02435a
Alvaro Sotelo Vila <asotelo@miatech.net>
2024-06-25 15:38:18 -0500

    AccountingMasterControl Creation

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/flown/AccountingMasterControlForm/AccountingMasterControlForm.js
@@ -0,0 +1,113 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+prototype.id = 'AccountingMasterControlForm';
+prototype.url = CONTEXTPATH + '/AccountingMasterControl';
+
+Ext.define('Ext.Praxis.view.flown.AccountingMasterControlForm.AccountingMasterControlForm', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.AccountingMasterControlForm',
+    requires: [
+        'Ext.Praxis.view.flown.AccountingMasterControlForm.Options',
+        'Ext.Praxis.view.flown.AccountingMasterControlForm.Filters',
+        'Ext.Praxis.view.flown.AccountingMasterControlForm.Info',
+        'Ext.Praxis.controller.flown.AccountingMasterControl.AccountingMasterControlController'
+    ],
+    controller: 'AccountingMasterControlController',
+    layout: {
+        type: 'fit'
+    },
+    padding: '0 0 0 0',
+    border: false,
+    defaults: {
+        border: false
+    },
+    items: [
+        {
+            id: prototype.id + '-xpanel',
+            border: false,
+            autoScroll: false,
+            layout: 'fit',
+            items: [
+                {
+                    id: prototype.id + '-form',
+                    border: false,
+                    bodyCls: 'colorFondo',
+                    layout: 'fit',
+                    defaults: {
+                        border: false,
+                        autoScroll: true
+                    },
+                    items: [
+                        {
+                            xtype: 'panel',
+                            region: 'center',
+//                          width: 900,
+                            layout: 'border',
+                            items: [
+                                {
+                                    region: 'center',
+                                    id: prototype.id + '-centerC',
+                                    layout: {
+                                        type: 'vbox',
+                                        align: 'center'
+                                    },
+                                    border: false,
+                                    autoScroll: true,
+                                    defaults: {
+                                        width: 1300,
+                                        align: 'center'
+                                    },
+                                    items: [
+                                        {
+                                            xtype: prototype.id + '-options'
+                                        }
+                                        ,
+                                        {
+                                            xtype: prototype.id + '-filters',
+                                            id: prototype.id + '-contentFilter'
+                                        }
+                                        ,
+                                        {
+                                            xtype: 'panel',
+                                            height: 650,
+                                            layout: 'fit',
+                                            items: [
+                                                {
+                                                    xtype: 'panel',
+                                                    id: prototype.id + '-centerC-panel01',
+                                                    layout: 'border',
+                                                    align: 'center',
+                                                    border: true,
+                                                    defaults: {
+                                                        border: false
+                                                    },
+                                                    bodyStyle: 'background-color: white;',
+                                                    items: [
+                                                        {
+                                                            region: 'center',
+                                                            xtype: prototype.id + '-info',
+                                                            id: prototype.id + '-contentInfo'
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+});
+
+
+
