77b8ffb4f74bb3aca0cb54d1d06b5eb940140870
Bryan Sauñe <bsaune@miatech.net>
2024-08-20 03:34:17 -0500

    changes by flight

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/flown/AverageFareForm/Filters.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/flown/AverageFareForm/Filters.js
@@ -150,7 +150,7 @@ Ext.define('Ext.Praxis.view.flown.AverageFareForm.Filters', {
                             store: new Ext.data.SimpleStore({
                                 fields: ['code', 'name'],
                                 data: [
-                                   ["F", "TKT"], ["E", "EMD"]
+                                   ["F", "TKT"], ["E", "EMD S"]
                                 ]
                             }),
                             valueField: 'code',
@@ -159,45 +159,78 @@ Ext.define('Ext.Praxis.view.flown.AverageFareForm.Filters', {
                         },
                         
                     ]
-                }
-//                {
-//                    xtype: 'panel',
-//                    bodyStyle: 'background: transparent',
-//                    id: prototype.id + '-panelFilters2',
-//                    border: false,
-//                    layout: 'column',
-//                    defaults: {
-//                        // labelStyle: 'font-weight:bold;',
-//                        fieldStyle: 'text-align: center;',
-//                        padding: '8px 7px 8px 10px',
-//                        anchor: '100%'
-//                    },
-//                    items: [
-//                        {
-//                            xtype: 'label',
-//                            text: 'Ticket',
-//                            width: 55,
+                },
+                {
+                    xtype: 'panel',
+                    bodyStyle: 'background: transparent',
+                    id: prototype.id + '-panelFilters2',
+                    border: false,
+                    layout: 'column',
+                    defaults: {
+                        // labelStyle: 'font-weight:bold;',
+                        fieldStyle: 'text-align: center;',
+                        padding: '8px 7px 8px 10px',
+                        anchor: '100%'
+                    },
+                    items: [
+//                        {xtype: 'tbspacer', width: 5},
+                        {
+                            xtype: 'label',
+                            text: 'Cod.Rea',
+                            width: 60,
 //                            style: 'font-weight:bold;',
-//                            padding: '10 5 5 5'
-//
-//                        },
-//                        {
-//                            xtype: 'textfield',
-//                            id: prototype.id + '-txtTKT',
-//                            required: true,
-//                            readOnly: false,
-//                            fieldLabel: '',
-//                            width: 120,
-//                            labelWidth: 0,
-//                            fieldStyle: 'text-align: center;',
-//                            labelAlign: 'left',
-//                            maxLength: 13,
-//                            enforceMaxLength: true,
-//                            maskRe: /[0-9]/
-//                        }
-//
-//                    ]
-//                }
+                            padding: '10 10 5 10'
+
+                        },
+                        {
+                            xtype: 'textfield',
+                            id: prototype.id + '-txtRFIC',
+                            required: true,
+                            readOnly: false,
+                            fieldLabel: '',
+                            width: 50,
+                            labelWidth: 0,
+                            fieldStyle: 'text-align: center;',
+                            labelAlign: 'left',
+                            maxLength: 1,
+                            enforceMaxLength: true,
+                            maskRe: /[a-zA-Z]/,
+                            listeners: {
+                                change: function(field, newValue) {
+                                    field.setValue(newValue.toUpperCase());
+                                }
+                            }
+                        },
+                        {
+                            xtype: 'label',
+                            text: 'Sub.Code',
+                            width: 55,
+//                            style: 'font-weight:bold;',
+                            padding: '10 5 5 5'
+
+                        },
+                        {
+                            xtype: 'textfield',
+                            id: prototype.id + '-txtRECODE',
+                            required: true,
+                            readOnly: false,
+                            fieldLabel: '',
+                            width: 78,
+                            labelWidth: 0,
+                            fieldStyle: 'text-align: center;',
+                            labelAlign: 'left',
+                            maxLength: 3,
+                            enforceMaxLength: true,
+                            maskRe: /[a-zA-Z0-9]/, 
+                            listeners: {
+                                change: function(field, newValue) {
+                                    field.setValue(newValue.toUpperCase());
+                                }
+                            }
+                        }
+
+                    ]
+                }
             ]
         }
     ]
