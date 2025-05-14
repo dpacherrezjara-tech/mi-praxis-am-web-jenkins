7aade4f04e0dfddbdaa77da93fbd9082dac0c5bc
Alvaro Sotelo Vila <asotelo@miatech.net>
2025-05-14 11:52:29 -0500

    Invoice Commission

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/sales/InvoiceCommissionConsortiaNoiataForm/InvoiceCommissionConsortiaNoiataForm.js
@@ -0,0 +1,113 @@
+
+
+prototype.id = 'InvoiceCommissionConsortiaNoiataForm';
+prototype.idLoadFileConsortia = 'LoadFileSubiArchivoConsortia';
+prototype.idLoadErrorFileConsortia = 'LoadErrorFileSubiArchivoConsortia';
+prototype.url = CONTEXTPATH + '/InvoiceCommissionConsortiaNoiata';
+
+Ext.define('Ext.Praxis.view.sales.InvoiceCommissionConsortiaNoiataForm.InvoiceCommissionConsortiaNoiataForm', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.InvoiceCommissionConsortiaNoiataForm',
+    requires: [
+        'Ext.Praxis.view.sales.InvoiceCommissionConsortiaNoiataForm.Options',
+        'Ext.Praxis.view.sales.InvoiceCommissionConsortiaNoiataForm.Filters',
+        'Ext.Praxis.view.sales.InvoiceCommissionConsortiaNoiataForm.Info',
+        'Ext.Praxis.view.sales.InvoiceCommissionConsortiaNoiataForm.LoadFile',
+        'Ext.Praxis.view.sales.InvoiceCommissionConsortiaNoiataForm.LoadErrorFile',
+        'Ext.Praxis.controller.sales.InvoiceCommissionConsortiaNoiata.InvoiceCommissionConsortiaNoiataController'
+    ],
+    controller: 'InvoiceCommissionConsortiaNoiataController',
+    layout: {
+        type: 'fit'
+    },
+    padding: '0 0 0 0',
+    border: false,
+    defaults: {
+        border: false
+    },
+    items: [
+        {
+            id: prototype.id + '-xpanel',
+            border: false,
+            autoScroll: false,
+            layout: 'fit',
+            items: [
+                {
+                    id: prototype.id + '-form',
+                    border: false,
+                    bodyCls: 'colorFondo',
+                    layout: 'fit',
+                    defaults: {
+                        border: false,
+                        autoScroll: true
+                    },
+                    items: [
+                        {
+                            xtype: 'panel',
+                            region: 'center',
+//                          width: 900,
+                            layout: 'border',
+                            items: [
+                                {
+                                    region: 'center',
+                                    id: prototype.id + '-centerC',
+                                    layout: {
+                                        type: 'vbox',
+                                        align: 'center'
+                                    },
+                                    border: true,
+                                    autoScroll: true,
+                                    defaults: {
+                                        //width: 1850,
+                                        width: 1300,
+                                        align: 'center'
+                                    },
+                                    items: [
+                                        {
+                                            xtype: prototype.id + '-options'
+                                        }
+                                        ,
+                                        {
+                                            xtype: prototype.id + '-filters',
+                                            id: prototype.id + '-contentFilter'
+                                        }
+                                        ,
+                                        {
+                                            xtype: 'panel',
+                                            height: 570,
+                                            // width: 1950,
+                                            layout: 'fit',
+                                            items: [
+                                                {
+                                                    xtype: 'panel',
+                                                    id: prototype.id + '-centerC-panel01',
+                                                    layout: 'border',
+                                                    align: 'center',
+                                                    border: true,
+                                                    defaults: {
+                                                        border: false
+                                                    },
+                                                    bodyStyle: 'background-color: white;',
+                                                    items: [
+                                                        {
+                                                            region: 'center',
+                                                            xtype: prototype.id + '-info',
+                                                            id: prototype.id + '-contentInfo'
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+});
+
+
+
