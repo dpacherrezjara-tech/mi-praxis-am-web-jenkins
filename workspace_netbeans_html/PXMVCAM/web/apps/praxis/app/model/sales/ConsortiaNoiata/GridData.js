e930e29329a1d41f55a182a074aef7ba12a91ea5
Alvaro Sotelo Vila <asotelo@miatech.net>
2025-05-14 10:13:07 -0500

    Sales Consortia Contract, Sales Consortia Noiata

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/model/sales/ConsortiaNoiata/GridData.js
@@ -0,0 +1,8 @@
+
+Ext.define('Ext.Praxis.model.sales.ConsortiaNoiata.GridData', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: '', type: 'string'},
+        {name: '', type: 'string'}
+    ]
+});
