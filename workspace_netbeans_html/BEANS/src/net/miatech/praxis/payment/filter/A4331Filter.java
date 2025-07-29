03810cd4fac44027fc5950a4b53e13bc74db4b32
DannPandal <dev.pandaldn@gmail.com>
2025-07-29 11:12:12 -0500

    fix: add difference for grid

--- workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A4331Filter.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A4331Filter.java
@@ -9,7 +9,15 @@ import net.miatech.praxis.payment.entities.A4331;
 public class A4331Filter extends A4331{
     private String DESC_ERROR,DES_MERCHANT,DES_SMERCHANT,DES_CERROR,
             DESC_CODADJU,DESC_PROCTYPE,BPOCOMENT,ADJUCOMENT,IDFLEX,PRAXISID,STMAIN;
-    private Double F_TAX,F_ADJUSMENT;
+    private Double F_TAX,F_ADJUSMENT, DIFFERENCE;
+
+    public Double getDIFFERENCE() {
+        return DIFFERENCE;
+    }
+
+    public void setDIFFERENCE(Double DIFFERENCE) {
+        this.DIFFERENCE = DIFFERENCE;
+    }
 
     public Double getF_ADJUSMENT() {
         return F_ADJUSMENT;
