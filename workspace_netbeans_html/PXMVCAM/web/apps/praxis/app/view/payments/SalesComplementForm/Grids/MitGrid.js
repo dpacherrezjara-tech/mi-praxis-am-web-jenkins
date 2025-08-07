683fb6486a3a909895bf8dd252e5eb293362b15e
Yamilet Remicio Gonzales <yadiraremiciogonzales@gmail.com>
2025-08-07 18:53:20 -0500

    fix width MIT

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesComplementForm/Grids/MitGrid.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/SalesComplementForm/Grids/MitGrid.js
@@ -97,7 +97,7 @@ Ext.define('Ext.Praxis.view.payments.SalesComplementForm.Grids.MitGrid', {
                 text: 'Type<br>Transaction', dataIndex: 'A4775TRXTP', width: 100
             },
             {
-                text: 'Issuing Bank', dataIndex: 'A4775BANCO', width: 180
+                text: 'Issuing Bank', dataIndex: 'A4775BANCO', width: 250
             },
             {
                 text: 'Number<br>Operation', dataIndex: 'A4775NROOP', width: 100
