f175d16a44aaff47b29c421701050c05c2dc3d54
Dann Pandal <dev.pandaldn@gmail.com>
2026-04-22 16:58:02 -0500

    fix: advance solve IND 2860

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/SalesReconciliationControl/SettlementMerchantGridController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/SalesReconciliationControl/SettlementMerchantGridController.js
@@ -18,17 +18,17 @@ Ext.define('Ext.Praxis.controller.payments.SalesReconciliationControl.Settlement
         const tdate = view.searchParams.IN_DATE === 'PRDA' ? 'Processing<br>Date' : 'Payment<br>Date';
         view.columns[0].setText(tdate);
         this.filters = view.searchParams ;
-        const res = await fetch(`${view.url}/loadSettlementSummary?${new URLSearchParams(view.searchParams)}`);
-        if (res.ok) {
-            const data = await res.json();
-            console.log(data);
+        try {
+            const res = await global.callStoreGet('PRAXISMP', 'SQP05133', view.searchParams);
+            const data = res?.lstRs?.[0] || [];
             let store = Ext.create('Ext.data.Store', {
-                data: data.response,
+                data: data,
                 autoLoad: true
             });
             view.setStore(store);
+        } finally {
+            view.unmask();
         }
-        view.unmask();
     },
     onClickMerchant: function (grid, td, rowIndex, cellIndex, e, record, tr, eOpts) {
         const me = this;
@@ -51,38 +51,100 @@ Ext.define('Ext.Praxis.controller.payments.SalesReconciliationControl.Settlement
     formatMerchantParams: function (obj) {
         const me = this;
         let params = Object.assign({}, me.view.searchParams);
-        params.IN_MERCHANT = obj.pmerchid;
-        params.IN_DATEFROM = obj.paydate ? obj.paydate : obj.prda;
-        params.IN_DATETO = obj.paydate ? obj.paydate : obj.prda;
-        params.IN_PROCTYPE = obj.proctype;
-        params.IN_PROCTYPESQ = obj.proctypesq;
-        params.IN_SCOUNTRY = obj.scountry;
-        params.IN_SCURRENCY = obj.scurrency;
-        params.IN_PCURRENCY = obj.pcurrency;
+        params.IN_MERCHANT = obj.PMERCHID;
+        params.IN_DATEFROM = obj.PAYDATE ? obj.PAYDATE : obj.PRDA;
+        params.IN_DATETO = obj.PAYDATE ? obj.PAYDATE : obj.PRDA;
+        params.IN_PROCTYPE = obj.PROCTYPE;
+        params.IN_PROCTYPESQ = obj.PROCTYPESQ;
+        params.IN_SCOUNTRY = obj.SCOUNTRY;
+        params.IN_SCURRENCY = obj.SCURRENCY;
+        params.IN_PCURRENCY = obj.PCURRENCY;
         params.IN_SCARDN = this.filters.IN_SCARDN ?? '';
         params.IN_AREFNBR = this.filters.IN_AREFNBR ?? '';
         params.IN_TICKET = this.filters.IN_TICKET ?? '';
+        params.IN_ARN = '';
         return params;
     },
-    downloadExcel:function(){
+    downloadExcel: function () {
         const me = this;
-        let params = Object.assign({}, me.view.searchParams);
-        params.excel = true;
-        console.log(params);
-        Ext.Msg.show(
-                {
-                    title: '.:PRAXIS:.',
-                    msg: 'Download Excel?',
-                    buttons: Ext.MessageBox.YESNO,
-                    scope: this,
-                    icon: Ext.MessageBox.QUESTION,
-                    modal: true,
-                    fn: function (btn) {
-                        if (btn === 'yes') {
-                            global.getFile(`${me.view.url}/downloadSettlementSummary?${new URLSearchParams(params)}`);
-                        }
+        const view = me.view;
+        const date = me.view.searchParams.IN_DATEFROM;
+        const nameFile = 'Settlement Merchant ' + date;
+
+        Ext.Msg.show({
+            title: '.:PRAXIS:.',
+            msg: 'Download Excel?',
+            buttons: Ext.MessageBox.YESNO,
+            scope: this,
+            icon: Ext.MessageBox.QUESTION,
+            modal: true,
+            fn: async function (btn) {
+                if (btn !== 'yes') {
+                    return;
+                }
+
+                const store = view.getStore();
+                let allRows = [];
+                if (store) {
+                    if (store.getData && Array.isArray(store.getData().items)) {
+                        allRows = store.getData().items;
                     }
+                }
+
+                const records = allRows.map(item => item && item.data ? item.data : item);
+                if (!records.length) {
+                    global.Msg({msg: 'No data to export'});
+                    return;
+                }
+
+                const colorAmounts = '#B2DAFA';
+                const colorPaymentInfo = '#FCF6DC';
+                let columns = [
+                    {
+                        title: 'Processing\nDate',
+                        valueGetter: function (row) { return row.PRDA || row.PAYDATE || ''; }
+                    },
+                    {title: 'Merchant', field: 'PMERCHID'},
+                    {title: 'Processor', field: 'DESC_PROCTYPE'},
+                    {title: 'Country', field: 'SCOUNTRY'},
+                    {title: 'Qty\nTransactions', field: 'QTYTRN', dataAlign: 'right'},
+                    {title: 'Currency', field: 'SCURRENCY'},
+                    {title: 'Total\nAmount', field: 'TGROSAMOUN', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'GROSS\nAmount', field: 'TGROSAMOUN_WCA', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'Comm.\nAmount', field: 'DISCAMOUN', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'Comm.\nVAT', field: 'DISCAMOUNI', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'Serv. Fee', field: 'SERVICEFEE', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'Serv. Fee\nVAT', field: 'OVERCOM12', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'CHBK\nAmount', field: 'TGROSAMOUN_CB', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'CHBK\nComm.', field: 'DISCAMOUN_CB', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'CHBK\nVAT', field: 'DISCAMOUNI_CB', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'ADJU\nAmount', field: 'ADJUSTMENT', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'ADJU\nComm.', field: 'DISCAMOUN_ADJ', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'ADJU\nVAT', field: 'DISCAMOUNI_ADJ', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'TAX', field: 'TAX', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'NET Amount', field: 'NETAMOUN', dataBgColor: colorAmounts, dataAlign: 'right'},
+                    {title: 'Payment Info.\nCurrency', field: 'PCURRENCY', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nTotal Amount', field: 'TGROSAMPAY', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nGROSS Amount', field: 'TGROSAMPAY_WCA', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nComm. Amount', field: 'SFEEAMOU', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nComm. VAT', field: 'IVACOM12', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nServ. Fee\nAmount', field: 'SERVICFEEP', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nServ. Fee\nVAT', field: 'OVERCOM12P', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nTAX', field: 'TAXP', dataBgColor: colorPaymentInfo, dataAlign: 'right'},
+                    {title: 'Payment Info.\nNET Amount\nTo receive AM', field: 'NETOPAY', dataBgColor: colorPaymentInfo, dataAlign: 'right'}
+                ];
+
+                await global.writeExcelFromJsonWithStyle({
+                    data: records,
+                    name: nameFile,
+                    columns: columns,
+                    defaultHeaderBgColor: '7F98A8',
+                    defaultHeaderFontColor: '000000',
+                    defaultDataBgColor: 'FFFFFF',
+                    defaultDataFontColor: '000000'
                 });
+            }
+        });
     }
 });
 
