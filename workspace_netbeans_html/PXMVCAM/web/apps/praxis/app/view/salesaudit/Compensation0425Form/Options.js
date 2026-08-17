b2e4ea6032a7f54f74b38ff9f7bcc8387e9e8b97
zenobio86 <zenobio86@gmail.com>
2026-08-17 17:54:55 -0500

    zpp cambios en la excel agrego monto y currency

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/salesaudit/Compensation0425Form/Options.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/salesaudit/Compensation0425Form/Options.js
@@ -26,7 +26,7 @@ Ext.define('Ext.Praxis.view.salesaudit.Compensation0425Form.Options', {
                         {
                             xtype: 'button',
                             id: prototype.id + '-btnAdd',
-                            //hidden: typeof gloUsr === 'undefined' || gloUsr !== 'DANAZCO' && gloUsr !== 'JLEYVA' && gloUsr !== 'FMARTINEZ' && gloUsr !== 'ANGELAG',
+                            hidden: typeof gloUsr === 'undefined' || gloUsr !== 'DANAZCO' && gloUsr !== 'JLEYVA' && gloUsr !== 'FMARTINEZ' && gloUsr !== 'ANGELAG',
                              iconCls: 'prx-icon-rules',
                             tooltip: 'Rule maintenance',
                             listeners: {
